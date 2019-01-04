0eabae6faa77d7a7f009109caef8115dd05c7191
pffsv <pffsv@mail.ru>
2019-01-04 11:41:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
+
+<!--Работа с формами в PHP*/
+    Пример формы-->
 <form action="" method="GET">
-	<input type="text" name="city">
+	<input type="text" name="user"><br><br>
+	<textarea name="message"></textarea><br><br>
+	<input type="submit"><br><br>
+</form>
+
+<!--1.Спросите имя пользователя с помощью формы. 
+Результат запишите в переменую $name. Выведите на экран фразу 'Привет, %Имя%'.-->
+<form action="" method="POST">
+	<input name="name" placeholder="введите имя">
+	<input type="submit" valua="отправить">	
+</form>
+
+<?php
+		$name = $_REQUEST['name'];
+		echo 'Привет, '.$name.'<br><br>';
+?>
+
+<!--1.Спросите имя пользователя с помощью формы. 
+Результат запишите в переменую $name. Выведите на экран фразу 'Привет, %Имя%'. Скрыть решение.-->
+<form action="" method="GET">
+	<input type="text" name="name">
 	<input type="submit">
 </form>
 
 <?php
-	//Если форма была отправлена и город не пустой:
-	if (!empty($_REQUEST['city'])) {
-		$city = $_REQUEST['city'];
-		echo 'Ваш город: '.$city;
+	if (isset($_REQUEST['name'])) {
+		$name = $_REQUEST['name'];
+		echo 'Привет,'.$name;
 	}
 ?>
