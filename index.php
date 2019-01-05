7eac36e780f2cd7d4b89801d3431db917b8d4fb5
pffsv <pffsv@mail.ru>
2019-01-05 09:27:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,28 @@
-
-<!--Спросите город пользователя с помощью формы. Результат запишите в переменную $city. 
-Выведите на экран фразу 'Ваш город: %Город%'.-->
-
-<form action="" method="GET">
-	<input type="text" name="city">
-	<input type="submit">
-</form>
-
-<?php
-//Если форма была отправлена и город не пустой:
-	if (!empty($_REQUEST['city'])) {
-		$city = $_REQUEST['city'];
-		echo "Ваш город".$city;
-   }
-?>
-<hr>
-
-<!--Решим предыдущую задачу так, чтобы пользователь не мог вводить теги-->
+<!--Спросите имя пользователя с помощью формы. Результат запишите в переменную $login. 
+Сделайте так, чтобы после отправки формы значения ее полей не пропадали.-->
 
 <form action="" method="GET">
-	<input type="text" name="city">
+	<input name="login" value="<?php if (isset($_GET['login'])) echo $_GET['login']; ?>">
 	<input type="submit">
 </form>
-
 <?php
-	//Если форма была отправлена и город не пустой:
-	if (isset($_REQUEST['city'])) {
-		$city = strip_tags($_REQUEST['city']);
-		echo 'Ваш город: '.$city;
-	}
-?>
-<hr>
+error_reporting(E_ALL);
+mb_internal_encoding("UTF-8");
+var_dump($_GET);
 
-<!--Давайте сделаем так, чтобы форма после отправки скрывалась-->
-
-<?php
-	//Если город пустой - покажем форму
-	if (isset($_REQUEST['city'])) {
-?>
-		<form action="" method="GET">
-			<input type="text" name="name">
-			<input type="submit">
-		</form>
-<?php
+	if (isset($_GET['submit'])) {
+		$login = $_GET['login'];
+		echo $login;
 	}
 ?>
 
-<?php
-	//Если форма была отправлена и город не пустой:
-	if (isset($_REQUEST['city'])) {
-		$city = strip_tags($_REQUEST['age']);
-		echo 'Ваш город: '.$age;
-	}
-?>
+<!-- Спросите у пользователя имя, а также попросите его ввести сообщение (textarea). 
+Сделайте так, чтобы после отправки формы значения его полей не пропадали-->
+
+<form action="" method="GET">
+	<input name="name" value="<?php if (isset($_GET['name'])) echo $_GET['name']; ?>">
+	<textarea name="message">
+		<?php if (isset($_GET['message'])) echo $_GET['message']; ?>
+	</textarea>
+	<input type="submit">
+</form>
