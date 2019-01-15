800ac27e7ac222ef70516b7a82c72defd2c12f8f
pffsv <pffsv@mail.ru>
2019-01-15 20:09:40 +0700

    Update train.php

--- train.php
+++ train.php
@@ -106,5 +106,89 @@
 		$key = $arr[$i];
 		$newArr[$key] = $arr[$i + 1];
 	}
-	var_dump($newArr);	
+	var_dump($newArr);
+	echo '<br>';	
+
+//29.Дана строка. Удалите из этой строки четные символы.
+
+	$str = 'aazzqqq';
+	$i = 0;
+	$res = '';
+	while ($i <= strlen($str)) {
+		$res .= substr($str, $i, 1);
+		$i += 2;
+	}
+	echo $res;
+	echo '<br>';
+
+/*30.Дана строка. Поменяйте ее первый символ на второй и наоборот, 
+третий на четвертый и наоборот, пятый на шестой и наоборот и так далее. 
+То есть из строки '12345678' нужно сделать '21436587'.*/
+
+	$str = '12345678';
+	$newStr = array_reverse(str_split(strrev($str), 2));
+	echo implode('', $newStr);
+	echo '<br>';
+
+//31.Сделайте аналог функции array_unique
+
+	function getArrUnique ($arr)
+	{
+		$result = [];
+		foreach ($arr as $elem) {
+			if (in_array($elem, $result) == false) {
+				$result[] = $elem;
+			}
+		}
+		return $result;
+	}
+
+//32.Сделайте функцию, противоположную функции array_unique. Ваша функция должна оставлять дубли и удалять элементы, не имеющие дублей. 
+
+	$arr = [1, 1, 1, 2, 3, 3, 4 ,5, 1, 6, 1, 3];
+	$newArr = [];
+	$elems = count($arr);
+	for ($i = 0; $i < $elems; $i++) {
+		$value = $arr[$i];
+		unset($arr[$i]);
+		if (in_array($value, $arr)) {
+			$newArr[] = $value;
+		}
+		$arr[$i] = $value;
+	}
+	$arr = $newArr;
+	echo var_dump($arr);
+	echo '<br>';
+
+//33.Напишите скрипт, который проверяет, является ли данное число простым (простое число - это то, которое делится только на 1 и на само себя).
+
+	$num = 31;
+	$flag = false;
+	for ($i = 2; $i < $num; $i++) {
+		if ($num % $i == 0) {
+			$flag = true;
+			break;
+		}
+	}
+
+	if ($flag == true) {
+		echo'Простое число';
+	} else {
+		echo 'Сложное число';
+	}
+	echo '<br>';
+
+//33.Дан массив со строками. Запишите в новый массив только те строки, которые начинаются с 'http://'
+
+	$arr = ['http://google.com', 'https://youtube.com', 'https://vk.com'];
+	$newArr = [];
+	foreach ($arr as $elem) {
+		$pos = strpos($elem, 'http://');
+		if ($pos !== false) {
+			$newArr[] = $elem;
+		}
+	}
+	
+	var_dump($newArr);
+
 ?>
