d6939567c04281eea3d5e604b8d8cf1d75971525
pffsv <pffsv@mail.ru>
2019-01-17 21:20:49 +0700

    Create homework.php

---
+++ homework.php
@@ -0,0 +1,38 @@
+<?php
+
+//1.exercise
+var_dump(2*2);
+var_dump(3/1);
+var_dump(1/3);
+var_dump('20cats' + 40);
+var_dump(18 % 4);
+echo '<br>';
+
+//2.exercise
+echo ($a = 2).'<br>';
+$x = ($y = 12) - 8; 
+echo $x.'<br>';
+
+//3.exercise
+
+var_dump(1 == 1.0);
+echo '<br>';
+
+var_dump(1 === 1.0);
+echo '<br>';
+
+var_dump('02' == 2);
+echo '<br>';
+
+var_dump('02' === 2);
+echo '<br>';
+
+var_dump('02' == '2');
+echo '<br>';
+
+//4.exercise
+$x = true xor true;
+var_dump($x);
+
+?>
+
