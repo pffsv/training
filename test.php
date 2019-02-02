846180d27854419e526728fb8f1625f1d353c8c1
pffsv <pffsv@mail.ru>
2019-02-02 15:26:50 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,25 +1,65 @@
-<?php
-/*
-	session_start(); 
-	if(isset($_SESSION['test'])) { 
-	echo $_SESSION['test']; 
-	}
-*/
+<table>
+	<tr>
+		<th>id</th>
+		<th>name</th>
+		<th>age</th>
+		<th>salary</th>
+		<th>delete</th>
+	</tr>
+	<?php
+		$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+		$user = 'root'; //имя пользователя, по умолчанию это root
+		$password = ''; //пароль, по умолчанию пустой
+		$db_name = 'test'; //имя базы данных
 
-	session_start(); 
+	//Соединяемся с базой данных используя наши доступы:
+		$link = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error($link));
 
-	if (isset($_SESSION['country'])) { 
-	echo 'Ваша страна: '.$_SESSION['country']; 
-	}
-
-
-	session_start(); 
-?> 
-
-<?php 
-session_start(); 
-?> 
-<form method="get" accept-charset="utf-8"> 
-<p>Имя: <input type="text" name="name"></p> 
-<p>Пароль: <input type="text" name="password"></p> 
-<p>E-mail: <input type="text" name="email" value="<?php if(isset($_SESSION['email'])): echo $_SESSION['email']; endif; ?>"></p> 
+	//Устанавливаем кодировку (не обязательно, но поможет избежать проблем):
+		mysqli_query($link, "SET NAMES 'utf8'");
+		// Сохранение нового (до получения!):
+		if (!empty($_POST)) {
+			$name = $_POST['name'];
+			$age = $_POST['age'];
+			$salary = $_POST['salary'];
+			
+			$query = "INSERT INTO workers SET name='$name', age='$age', salary='$salary'";
+			mysqli_query($link, $query) or die(mysqli_error($link));
+		}
+		
+		// Удаление по id (до получения!):
+		if (isset($_GET['del'])) {
+			$del = $_GET['del'];
+			$query = "DELETE FROM workers WHERE id=$del";
+			mysqli_query($link, $query) or die(mysqli_error($link));
+		}
+	
+		// Получение всех работников:
+		$query = "SELECT * FROM workers";
+		$result = mysqli_query($link, $query) or die(mysqli_error($link));
+		for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+		
+		// Вывод на экран:
+		$result = '';
+		foreach ($data as $elem) {
+			$result .= '<tr>';
+			
+			$result .= '<td>' . $elem['id'] . '</td>';
+			$result .= '<td>' . $elem['name'] . '</td>';
+			$result .= '<td>' . $elem['age'] . '</td>';
+			$result .= '<td>' . $elem['salary'] . '</td>';
+			$result .= '<td>' . $elem['salary'] . '</td>';
+			$result .= '<td><a href="?del=' . $elem['id'] . '">удалить</a></td>';
+			
+			$result .= '</tr>';
+		}
+		
+		echo $result;
+	?>
+</table>
+<form action="" method="POST">
+	<input name="name">
+	<input name="age">
+	<input name="salary">
+	<input type="submit" value="добавить работника">
+</form>
