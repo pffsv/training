e4d7be4cefa5335e904743922b9c0b0bcc30a638
pffsv <pffsv@mail.ru>
2019-02-04 20:07:34 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,241 +1,182 @@
 
 <!-- 
-43.Практика PHP
+44.Минипроект: гостевая книга	
+Урок 1
+По следующей ссылке находится архив: скачать. В данном архиве находится верстка гостевой книги. 
+Ваша задача - взять эту верстку и оживить ее на PHP.
 
-	
-	 3. Сделайте форму добавления нового работника.
+Как это должно работать: посетитель нашего сайта заходит на страницу, вбивает свое имя и сообщение, 
+нажимает на кнопку - форма отправляется, сообщение добавляется и появляется сверху в сообщениях, 
+при этом над формой должно вывестись сообщение об успешном сохранении.
 
-	 4. Сделайте колонку 'редактировать' для каждого работника. Там должна быть ссылка, по переходу на которую появится страница с формой редактирования работника.
-
-	 5. Над таблицей с работниками сделайте инпут, в который вводится зарплата. По нажатию на кнопку следует вывести таблицу работников с введенной зарплатой.
-
-	 7. Сделайте колонку 'удалить', в которой для каждого работника будет стоять чекбокс. Под таблицей сделайте кнопку, по нажатию на которую будут удалены те работники, для которых чекбокс был отмечен.
+Над формой должны выводится сообщения, оставленные предыдущими посетителями сайта 
+(видны всегда, отсортированы по убыванию даты, то есть в начале самые новые).
 	 
-	 У тебя 6 задание, то же, что и 5
 -->
 
 <?php
-	$host = 'localhost'; 
-	$user = 'root'; 
-	$password = ''; 
-	$db_name = 'test';
 
-	
+	$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+	$user = 'root'; //имя пользователя, по умолчанию это root
+	$password = ''; //пароль, по умолчанию пустой
+	$db_name = 'test'; //имя базы данных
+
+	//Соединяемся с базой данных используя наши доступы:
 	$link = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error($link));
 
 	
 	mysqli_query($link, "SET NAMES 'utf8'");
-?>
-
-
-
-<form action="" method="get">
-	<label>Искать работника, с зарплатой <input type="text" name="user-salary"></label>
-	<input type="submit" value="Найти" name="search">
-</form>
-
-<?php
-	if(!empty($_GET['user-salary'])) {
 	
-		$query = "SELECT * FROM `workers` WHERE salary =" . $_GET['user-salary'];
-		
-		$result = mysqli_query($link, $query) or die(mysqli_error($link));
-
-		
-		for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-		
-		if(count($data) == 0) {
-			echo 'Работника с такой зарплатой, не существует';
-		}
-		
-		else {
-			echo '<table border="1" width="50%" cellpadding="5" style="border-collapse: collapse; text-align: center;">';
-			echo '<tr>';
-			foreach($data[0] as $key => $val) {
-				echo '<th>'.$key.'</th>';
-			}
-			echo '</tr>';
-			
-			foreach($data as $arr) {
-				echo '<tr>';
-				foreach($arr as $k => $v) {
-					echo '<td>'.$v.'</td>';
-				}
-				echo '</tr>';
-			}
-			
-		echo '</table>';
-		}
-		
-		echo '<br><br>';
-	}
-?>
-
-<hr>
-
-<?php
-		
-		$query = "SELECT * FROM workers WHERE id > 0";
+	$allRecord = "SELECT * FROM `records`";
 	
-		$result = mysqli_query($link, $query) or die(mysqli_error($link));
+	$result = mysqli_query($link, $allRecord) or die(mysqli_error($link));
 
 		
-		for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-		
-		
-		echo '<form action="" method="post">';
-		
-		echo '<table border="1" width="50%" cellpadding="5" style="border-collapse: collapse; text-align: center;">';
-			echo '<tr>';
-			foreach($data[0] as $key => $val) {
-				echo '<th>'.$key.'</th>';
-			}
-			
-			echo '<th colspan="3">действие</th>';
-			echo '</tr>';
-			
-			foreach($data as $arr) {
-				echo '<tr>';
-				foreach($arr as $k => $v) {
-					echo '<td>'.$v.'</td>';
-				}
-				
-				echo '<td><a href="?del='.$arr['id'].'">удалить</a></td>';
-				echo '<td><a href="?red='.$arr['id'].'">редактировать</a></td>';
-				echo '<td><input type="checkbox" name="user[]" value="'.$arr['id'].'"></td>';
-				
-				echo '</tr>';
-			}
-			
-		echo '</table><br>';
-		
-		echo '<input type="submit" name="del-several-user" value="Удалить отмеченых работников">';
-		
-		echo '</form>';
-		
-		
-		if(!empty($_POST['del-several-user'])) {
-			array_pop($_POST);
-			
-			$str = implode(', ', $_POST['user']);
-			
-			$delUser = "DELETE FROM `workers` WHERE id IN (".$str.")";
-			
-			mysqli_query($link, $delUser) or die(mysqli_error($link));
-			
-			header('Location: '.$_SERVER['PHP_SELF']);
-		}
-		
-		
-		if(!empty($_GET['del'])) {
-			$queryDelUser = "DELETE FROM `workers` WHERE id = ".$_GET['del'];
-			
-			mysqli_query($link, $queryDelUser) or die(mysqli_error($link));
-			
-			header('Location: '.$_SERVER['PHP_SELF']);
-		}
-		
-		
-		if(!empty($_GET['showaddform'])) {
-			echo '<br>';
-			echo '<form method="POST" action="" name="new-rab">';?>
-				<label>Имя: <input type="text" name="worker" value="<?php if(!empty($_POST['worker'])) echo $_POST['worker'];?>"></label><br>
-				<label>Возраст: <input type="text" name="age" value="<?php if(!empty($_POST['age'])) echo $_POST['age'];?>"></label><br>
-				<label>Зарплата: <input type="text" name="salary" value="<?php if(!empty($_POST['salary'])) echo $_POST['salary'];?>"></label><br>
-				<br>
-				<input type="submit" value="Отправить" name="btn-add-worker">
-				&nbsp &nbsp &nbsp
-				<a href="<?php echo $_SERVER['PHP_SELF']?>">Вернутья назад</a>
-			</form>
-		<?php
-		}
-		
-		
-		else {
-			echo '<br>';
-			echo '<a href="'.$_SERVER['PHP_SELF'].'/?showaddform=ok">Добавить нового работника</a>';
-		}
-		
-		if(!empty($_POST['btn-add-worker'])) {
-		
-			if(empty($_POST['worker']))	echo '<p style="color: red; font-weight: bold;">Введите имя</p>';
-			
-			if(empty($_POST['age']))	echo '<p style="color: red; font-weight: bold;">Введите возраст</p>';
-			
-			if(empty($_POST['salary'])) echo '<p style="color: red; font-weight: bold;">Введите зарплату</p>';
-			
-			
-			else {
-				$name = $_POST['worker'];
-				$age = $_POST['age'];
-				$salary = $_POST['salary'];
-				
-				$newWorker = 'INSERT INTO `workers` SET name="'.$name.'", age="'.$age.'", salary="'.$salary. '"';
-				
-				mysqli_query($link, $newWorker) or die(mysqli_error($link));
-				
-				header('Location: '.$_SERVER['PHP_SELF']);
-			}
-		
-		}
-		
-		
-		
-		
-		if(!empty($_GET['red'])) {
-			
-			$infoUser = "SELECT * FROM `workers` WHERE id =".$_GET['red'];
-			$result = mysqli_query($link, $infoUser) or die(mysqli_error($link));
-			
-			$res = mysqli_fetch_assoc($result);
-			
-			echo '<br><br>';
-			
-			echo '<div style="border: 1px solid; width: 500px;">';
-			
-			echo 'Редактирование работника';
-			
-			echo '<br><br>';
-			
-			echo '<form method="POST" action="" name="new-rab">';?>
-				<label>Имя: <input type="text" name="worker" value="<?php echo $res['name'];?>"></label><br>
-				<label>Возраст: <input type="text" name="age" value="<?php echo $res['age'];?>"></label><br>
-				<label>Зарплата: <input type="text" name="salary" value="<?php echo $res['salary'];?>"></label><br>
-				<br>
-				<input type="submit" value="Отправить" name="btn-red-worker">
-				&nbsp &nbsp &nbsp
-				<a href="<?php echo $_SERVER['PHP_SELF']?>">Вернутья назад</a>
-			</form>
-			
-			<div>
-		<?php
-		}
-		
-		
-		if(!empty($_POST['btn-red-worker'])) {
-		
-			if(empty($_POST['worker']))	echo '<p style="color: red; font-weight: bold;">Введите имя</p>';
-			
-			if(empty($_POST['age']))	echo '<p style="color: red; font-weight: bold;">Введите возраст</p>';
-			
-			if(empty($_POST['salary'])) echo '<p style="color: red; font-weight: bold;">Введите зарплату</p>';
-			
-			
-			else {
-				$id = $res['id'];
-				$name = $_POST['worker'];
-				$age = $_POST['age'];
-				$salary = $_POST['salary'];
-				
-				$newWorker = 'UPDATE `workers` SET name="'.$name.'", age="'.$age.'", salary="'.$salary. '" WHERE id =' . $id;
-				
-				mysqli_query($link, $newWorker) or die(mysqli_error($link));
-				
-				header('Location: '.$_SERVER['PHP_SELF']);
-			}
-		
-		}
-		
-		
+	for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+	
+
 ?>
 
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Гостевая книга</title>
+		<meta charset="utf-8">
+		
+		<style>
+			.main {
+				width: 500px;
+				margin: 0 auto;
+				padding: 10px;
+				box-sizing: border-box;
+			}
+			
+			.data-name, .rec, .user-name, .user-text {
+				margin-bottom: 10px;
+			}
+			
+			.data {
+				font-weight: bold;
+			}
+			
+			.user-name, .user-text {
+				width: 100%;
+				padding: 10px;
+				box-sizing: border-box;
+				border: 1px solid #ccc;
+				border-radius: 5px;
+				box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+				font-size: 14px;
+			}
+			
+			.user-text {
+				min-height: 200px;
+			}
+			
+			.btn-save {
+				width: 100%;
+				display: block;
+				background-color: #5bc0de;
+				border-color: #46b8da;
+				color: white;
+				padding: 6px 12px;
+				border: 1px solid transparent;
+				box-sizing: border-box;
+				border-radius: 5px;
+			}
+			
+			.message {
+				color: #31708f;
+				background-color: #d9edf7;
+				border-color: #bce8f1;
+				border-radius: 5px;
+				padding: 15px;
+				margin-bottom: 10px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="main">
+			<h1>Гостевая книга</h1>
+			
+			<div class="records">
+				<?php
+					foreach($data as $key) {
+						echo '<div class="rec">';
+						echo '<div class="data-name">';
+							echo '<span class="data">'.date("d.m.Y G:i:s", strtotime($key['data'])).' </span>';
+							echo '<span class="name">'.$key['name'].'</span>';
+						echo '</div>';
+						
+						echo '<div class="text">';
+							echo $key['text'];
+						echo '</div>';
+						
+					echo '</div>';
+					}
+					
+				?>
+				
+			</div>
+			
+			
+				<?php
+					if(!empty($_POST['user-name']) && !empty($_POST['user-text'])) {
+					
+						$name = $_POST['user-name'];
+						$text = $_POST['user-text'];
+						$nowData = date("Y-m-d G:i:s");
+				
+						$sql = 'INSERT INTO `records` SET name="'.$name.'", text="'.$text.'", data="'.$nowData. '"';
+						
+						mysqli_query($link, $sql) or die(mysqli_error($link));
+				
+						echo '<div class="message">';
+							echo 'Запись успешно сохранена!';
+						echo '</div>';
+					}
+				?>
+			
+			
+			
+			<div class="add-record">
+				<form action="" method="post">
+					<?php
+						if(!empty($_POST['btn-save']) && empty($_POST['user-name'])) {
+							
+								echo '<span style="color: red;">Введите имя</span><br>';
+								echo '<input type="text" class="user-name" name="user-name" placeholder="Ваше имя">';
+						}
+						else {?>
+							<input type="text" class="user-name" name="user-name" value="<?php if(!empty($_POST['user-name'])) echo $_POST['user-name'];?>" placeholder="Ваше имя">
+						<?php	
+						}
+					?>
+					
+					<?php
+						if(!empty($_POST['btn-save']) && empty($_POST['user-text'])) {
+							
+								echo '<span style="color: red;">Введите текст</span><br>';
+								echo '<textarea  class="user-text" name="user-text" name="text" placeholder="Ваш отзыв"></textarea>';
+							
+						}
+						else {?>
+							<textarea  class="user-text" name="user-text" name="text" placeholder="Ваш отзыв"><?php if(!empty($_POST['user-text'])) echo $_POST['user-text'];?></textarea>
+							<?php
+						}
+						
+						
+					?>
+					
+					<input type="submit" class="btn-save" name="btn-save" value="Сохранить">
+				</form>
+				
+				
+				
+			</div>
+			
+			
+		</div>
+	</body> 
+</html>
