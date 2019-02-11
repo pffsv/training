f65f08a975da875b790c9c46f8deeefbbf4acfc3
pffsv <pffsv@mail.ru>
2019-02-11 21:23:06 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,7 +1,9 @@
-3. Задача:
-Модифицируйте код так, чтобы в случае успешной авторизации происходил редирект на страницу index.php
+4. Задача:
+Модифицируйте код так, чтобы на странице index.php выводилось сообщение об успешной авторизации. Решите задачу через флеш-сообщения на сессиях (про них было в простом движке php)
 
+Файл login.php 
 <?php 
+//session_start(); 
 $local = 'localhost'; 
 $user = 'root'; 
 $password = ''; 
@@ -20,10 +22,12 @@ $result = mysqli_query($connect, $query) or die (mysqli_error($connect));
 $user = mysqli_fetch_assoc($result); 
 
 if (!empty($user)) { 
+$_SESSION['showResult'] = "Авторизация прошла успешно"; 
 header('Location: index.php'); 
 } 
 else { 
-echo 'Wrong '; 
+$_SESSION['showResult'] = "Ошибка. Пользователь не авторизован"; 
+header('Location: index.php'); 
 } 
 } 
 ?> 
@@ -31,4 +35,27 @@ echo 'Wrong ';
 <input name="login" placeholder="login"> 
 <input type="password" name="password" placeholder="password"> 
 <input type="submit"> 
-</form>
+</form> 
+
+Файл index.php 
+<?php 
+//session_start(); 
+?> 
+<!DOCTYPE html> 
+<html> 
+<head> 
+<meta charset="utf-8"> 
+<title>Учу PHP :)</title> 
+<style> 
+</style> 
+</head> 
+<body> 
+<? 
+if (isset($_SESSION['showResult'])) { 
+echo $_SESSION['showResult']; 
+
+unset($_SESSION['showResult']); 
+} 
+?> 
+</body> 
+</html>
