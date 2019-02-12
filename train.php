1db88ecfd58167b32c9c1e202c0dc50f6eabe6c0
pffsv <pffsv@mail.ru>
2019-02-12 20:26:18 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,9 +1,4 @@
-4. Задача:
-Модифицируйте код так, чтобы на странице index.php выводилось сообщение об успешной авторизации. Решите задачу через флеш-сообщения на сессиях (про них было в простом движке php)
-
-Файл login.php 
 <?php 
-//session_start(); 
 $local = 'localhost'; 
 $user = 'root'; 
 $password = ''; 
@@ -12,50 +7,17 @@ $db_base = 'test';
 $connect = mysqli_connect($local, $user, $password, $db_base) or die (mysqli_error($connect)); 
 mysqli_query($connect, "SET NAMES 'utf8'"); 
 
-if (!empty($_POST['login']) AND !empty($_POST['password'])) { 
-$login = $_POST['login']; 
-$password = $_POST['password']; 
+if (!empty($_POST['addLogin']) && !empty($_POST['addPassword'])) { 
+$addLogin = $_POST['addLogin']; 
+$addPassword = $_POST['addPassword']; 
 
-$query = "SELECT * FROM users WHERE login='$login' AND password='$password'"; 
+$query = "INSERT INTO users SET login = '$addLogin', password = '$addPassword'"; 
 $result = mysqli_query($connect, $query) or die (mysqli_error($connect)); 
-
-$user = mysqli_fetch_assoc($result); 
-
-if (!empty($user)) { 
-$_SESSION['showResult'] = "Авторизация прошла успешно"; 
-header('Location: index.php'); 
-} 
-else { 
-$_SESSION['showResult'] = "Ошибка. Пользователь не авторизован"; 
-header('Location: index.php'); 
-} 
 } 
 ?> 
+
 <form action="" method="POST"> 
-<input name="login" placeholder="login"> 
-<input type="password" name="password" placeholder="password"> 
+<input name="addLogin" placeholder="enter login"> 
+<input type="password" name="addPassword" placeholder="enter password"> 
 <input type="submit"> 
-</form> 
-
-Файл index.php 
-<?php 
-//session_start(); 
-?> 
-<!DOCTYPE html> 
-<html> 
-<head> 
-<meta charset="utf-8"> 
-<title>Учу PHP :)</title> 
-<style> 
-</style> 
-</head> 
-<body> 
-<? 
-if (isset($_SESSION['showResult'])) { 
-echo $_SESSION['showResult']; 
-
-unset($_SESSION['showResult']); 
-} 
-?> 
-</body> 
-</html>
+</form>
