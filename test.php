830413ee8fa9b74697212362921119ac4c4ec4e1
pffsv <pffsv@mail.ru>
2019-02-12 20:20:22 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,65 +1,23 @@
-<table>
-	<tr>
-		<th>id</th>
-		<th>name</th>
-		<th>age</th>
-		<th>salary</th>
-		<th>delete</th>
-	</tr>
-	<?php
+	//Устанавливаем доступы к базе данных:
 		$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
 		$user = 'root'; //имя пользователя, по умолчанию это root
 		$password = ''; //пароль, по умолчанию пустой
 		$db_name = 'test'; //имя базы данных
 
 	//Соединяемся с базой данных используя наши доступы:
-		$link = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error($link));
+		$link = mysqli_connect($host, $user, $password, $db_name);
 
 	//Устанавливаем кодировку (не обязательно, но поможет избежать проблем):
 		mysqli_query($link, "SET NAMES 'utf8'");
-		// Сохранение нового (до получения!):
-		if (!empty($_POST)) {
-			$name = $_POST['name'];
-			$age = $_POST['age'];
-			$salary = $_POST['salary'];
-			
-			$query = "INSERT INTO workers SET name='$name', age='$age', salary='$salary'";
-			mysqli_query($link, $query) or die(mysqli_error($link));
-		}
-		
-		// Удаление по id (до получения!):
-		if (isset($_GET['del'])) {
-			$del = $_GET['del'];
-			$query = "DELETE FROM workers WHERE id=$del";
-			mysqli_query($link, $query) or die(mysqli_error($link));
-		}
-	
-		// Получение всех работников:
-		$query = "SELECT * FROM workers";
+
+	//Формируем тестовый запрос:
+		$query = "SELECT * FROM workers WHERE id > 0";
+
+	//Делаем запрос к БД, результат запроса пишем в $result:
 		$result = mysqli_query($link, $query) or die(mysqli_error($link));
+
 		for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-		
-		// Вывод на экран:
-		$result = '';
-		foreach ($data as $elem) {
-			$result .= '<tr>';
-			
-			$result .= '<td>' . $elem['id'] . '</td>';
-			$result .= '<td>' . $elem['name'] . '</td>';
-			$result .= '<td>' . $elem['age'] . '</td>';
-			$result .= '<td>' . $elem['salary'] . '</td>';
-			$result .= '<td>' . $elem['salary'] . '</td>';
-			$result .= '<td><a href="?del=' . $elem['id'] . '">удалить</a></td>';
-			
-			$result .= '</tr>';
-		}
-		
-		echo $result;
-	?>
-</table>
-<form action="" method="POST">
-	<input name="name">
-	<input name="age">
-	<input name="salary">
-	<input type="submit" value="добавить работника">
-</form>
+
+
+	//Проверяем что же нам отдала база данных, если null – то какие-то проблемы:
+		var_dump($data);
