45d13fa7b4ae8ce241490b3bdbded5889322cb96
pffsv <pffsv@mail.ru>
2019-02-13 20:50:31 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,7 +1,9 @@
 
 <?php 
-//5. Задача
-//Запишите при регистрации в сессию еще и id пользователя
+/*6. Задача
+Модифицируйте ваш код так, чтобы при отправке формы пароль сравнивался с его подтверждением. 
+Если они совпадают - то продолжаем регистрацию, а если не совпадают - то выводим сообщение об этом */
+
 session_start(); 
 
 $local = 'localhost'; 
@@ -12,15 +14,13 @@ $db_base = 'test';
 $connect = mysqli_connect($local, $user, $password, $db_base) or die (mysqli_error($connect)); 
 mysqli_query($connect, "SET NAMES 'utf8'"); 
 
-if (!empty($_POST['addLogin']) && !empty($_POST['addPassword']) && !empty($_POST['addBirthday']) && !empty($_POST['addEmail'])) { 
+if (!empty($_POST['addLogin']) && !empty($_POST['addPassword']) && !empty($_POST['confirm'])) { 
+if ($_POST['addPassword'] == $_POST['confirm']) { 
 $addLogin = $_POST['addLogin']; 
 $addPassword = $_POST['addPassword']; 
-$addBirthday = $_POST['addBirthday']; 
-$addEmail = $_POST['addEmail']; 
 
 $date = date('Y-m-d'); 
-$query = "INSERT INTO users SET 
-login = '$addLogin', password = '$addPassword', birthday = '$addBirthday', email = '$addEmail', registration_date = '$date'"; 
+$query = "INSERT INTO users SET login = '$addLogin', password = '$addPassword', registration_date = '$date'"; 
 $result = mysqli_query($connect, $query) or die (mysqli_error($connect)); 
 
 $_SESSION['auth'] = true; 
@@ -31,12 +31,14 @@ $_SESSION['id'] = $id;
 
 header('Location: index.php'); 
 } 
+else { 
+echo "<p style=\"color:red\"> enter the correct password </p>"; 
+} 
+} 
 ?> 
-
 <form action="" method="POST"> 
 <input name="addLogin" placeholder="enter login"> 
 <input type="password" name="addPassword" placeholder="enter password"> 
-<input name="addBirthday" placeholder="enter your date of birth"> 
-<input name="addEmail" placeholder="enter your email"> 
+<input type="password" name="confirm" placeholder="confirm your password"> 
 <input type="submit"> 
 </form>
