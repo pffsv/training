746ffd8920cad81e1229cc1c9e634e79b76d420c
pffsv <pffsv@mail.ru>
2019-02-13 20:52:00 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,8 +1,8 @@
 
 <?php 
-/*6. Задача
-Модифицируйте ваш код так, чтобы при отправке формы пароль сравнивался с его подтверждением. 
-Если они совпадают - то продолжаем регистрацию, а если не совпадают - то выводим сообщение об этом */
+/*7. Задача
+Модифицируйте ваш код так, чтобы при попытке регистрации выполнялась проверка на занятость логина и, 
+если он занят, - выводите сообщение об этом и просите ввести другой логин*/
 
 session_start(); 
 
@@ -19,6 +19,11 @@ if ($_POST['addPassword'] == $_POST['confirm']) {
 $addLogin = $_POST['addLogin']; 
 $addPassword = $_POST['addPassword']; 
 
+$query = "SELECT * FROM users WHERE login = '$addLogin'"; 
+$checkLogin = mysqli_query($connect, $query) or die (mysqli_error($connect)); 
+$user = mysqli_fetch_assoc($checkLogin); 
+
+if (empty($user)) { 
 $date = date('Y-m-d'); 
 $query = "INSERT INTO users SET login = '$addLogin', password = '$addPassword', registration_date = '$date'"; 
 $result = mysqli_query($connect, $query) or die (mysqli_error($connect)); 
@@ -32,6 +37,10 @@ $_SESSION['id'] = $id;
 header('Location: index.php'); 
 } 
 else { 
+echo "<p style=\"color:red\"> username already exists </p>"; 
+} 
+} 
+else { 
 echo "<p style=\"color:red\"> enter the correct password </p>"; 
 } 
 } 
@@ -40,5 +49,5 @@ echo "<p style=\"color:red\"> enter the correct password </p>";
 <input name="addLogin" placeholder="enter login"> 
 <input type="password" name="addPassword" placeholder="enter password"> 
 <input type="password" name="confirm" placeholder="confirm your password"> 
-<input type="submit"> 
+<input type="submit" value="Register"> 
 </form>
