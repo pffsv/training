ff74748b4c15f44d9580b5e3bcb6b2d7d1fc0256
pffsv <pffsv@mail.ru>
2019-02-15 21:00:29 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,5 +1,6 @@
+<!--4. Задача
+Реализуйте описанную выше авторизацию с соленым паролем. Попробуйте зарегистрируйтесь, авторизуйтесь, убедитесь, что все работает-->
 
-Спросите у пользователя при регистрации еще и email. Занесите его в базу данных. Выполните проверку емейла на корректность и, если он некорректен, над соответствующим инпутом выведите сообщение об этом
 <?php 
 session_start(); 
 
@@ -11,75 +12,37 @@ $db_base = 'test';
 $connect = mysqli_connect($local, $user, $password, $db_base) or die (mysqli_error($connect)); 
 mysqli_query($connect, "SET NAMES 'utf8'"); 
 
-$loginText = ''; 
+if (!empty($_POST['login']) AND !empty($_POST['password'])) { 
+$login = $_POST['login']; 
 
-if (!empty($_POST['addLogin']) && !empty($_POST['addPassword']) && !empty($_POST['confirm']) && !empty($_POST['addEmail'])) { 
-if ($_POST['addPassword'] == $_POST['confirm']) { 
-if (!empty(trim($_POST['addLogin'])) AND !empty(trim($_POST['addPassword'])) AND !empty(trim($_POST['confirm'])) AND !empty(trim($_POST['addEmail']))) { 
-$regLogin = "#^[A-Za-z\d]+$#"; 
-$regEmail = "#^[A-Za-z\d]+@[a-z]+\.[a-z]{2,}$#"; 
-if (preg_match($regLogin, $_POST['addLogin'])) { 
-if (strlen($_POST['addLogin']) >= 4 && strlen($_POST['addLogin']) <= 10) { 
-if (strlen($_POST['addPassword']) >= 6 && strlen($_POST['addPassword']) <= 12) { 
-if (preg_match($regEmail, $_POST['addEmail'])) { 
-$addLogin = $_POST['addLogin']; 
-$addPassword = $_POST['addPassword']; 
-$addEmail = $_POST['addEmail']; 
-
-$query = "SELECT * FROM users WHERE login = '$addLogin'"; 
-$checkLogin = mysqli_query($connect, $query) or die (mysqli_error($connect)); 
-$user = mysqli_fetch_assoc($checkLogin); 
-
-if (empty($user)) { 
-$date = date('Y-m-d'); 
-$query = "INSERT INTO users SET login = '$addLogin', password = '$addPassword', email='$addEmail', registration_date = '$date'"; 
+$query = "SELECT * FROM users WHERE login='$login'"; 
 $result = mysqli_query($connect, $query) or die (mysqli_error($connect)); 
+$user = mysqli_fetch_assoc($result); 
 
+if (!empty($user)) { 
+$passwordEntered = md5($user['salt'].$_POST['password']); 
+$passwordFromDatabase = $user['password']; 
+
+if ($passwordEntered == $passwordFromDatabase) { 
 $_SESSION['auth'] = true; 
-$_SESSION['login'] = $addLogin; 
-
-$id = mysqli_insert_id($connect); 
-$_SESSION['id'] = $id; 
-
-header('Location: index.php'); 
+$_SESSION['login'] = $login; 
+header ('Location: index.php'); 
 } 
 else { 
-$loginText = "<p style=\"color:red\"> username already exists </p>"; 
+$_SESSION['auth'] = null; 
+echo "<p> Повторите попытку </p>"; 
 } 
 } 
 else { 
-$emailText = "<p style=\"color:red\"> enter the correct email </p>"; 
-} 
-} 
-else { 
-$passwordText = "<p style=\"color:red\"> Your password must contain from 6 to 12 characters </p>"; 
-} 
-} 
-else { 
-$loginText = "<p style=\"color:red\"> Your login must contain from 4 to 10 characters </p>"; 
-} 
-} 
-else { 
-$loginText = "<p style=\"color:red\"> Login can contain only Latin letters or numbers </p>"; 
-} 
-} 
-else { 
-$loginText = "<p style=\"color:red\"> Enter a valid username or password or email. Must not contain an empty string </p>"; 
-} 
-}
-else { 
-$passwordText = "<p style=\"color:red\"> enter the correct password </p>"; 
+echo "<p> Повторите попытку </p>"; 
 } 
 } 
 ?> 
+<head> 
+<meta charset="utf-8"> 
+</head> 
 <form action="" method="POST"> 
-<p> <?= $loginText; ?> </p> 
-<input name="addLogin" placeholder="enter login" value="<?php if(!empty($_POST['addLogin'])) echo $_POST['addLogin']; ?>"> 
-<p> <?= $passwordText ?> </p> 
-<input type="password" name="addPassword" placeholder="enter password" value="<?php if(!empty($_POST['addPassword'])) echo $_POST['addPassword']; ?>"> 
-<p> </p> 
-<input type="password" name="confirm" placeholder="confirm your password" value="<?php if(!empty($_POST['confirm'])) echo $_POST['confirm']; ?>"> 
-<p> <?= $emailText ?> </p> 
-<input name="addEmail" placeholder="enter email" value="<?php if(!empty($_POST['addEmail'])) echo $_POST['addEmail']; ?>"><br><br> 
-<input type="submit" value="Register"> 
+<input name="login" placeholder="login"> 
+<input type="password" name="password" placeholder="password"> 
+<input type="submit" value="Авторизировать"> 
 </form>
