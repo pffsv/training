5455b41aed09ba21df07e8f60118970e7f77f047
pffsv <pffsv@mail.ru>
2019-02-16 15:29:53 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,44 +1,41 @@
 <?php 
-session_start(); 
-if (!empty($_SESSION['auth'])) { 
-$_SESSION['auth'] = true; 
-} 
-else { 
-$_SESSION['auth'] = null; 
-} 
-?> 
-<!DOCTYPE html> 
-<html> 
-<head> 
-<meta charset="utf-8"> 
-<title>Учу PHP :)</title> 
-<style> 
-</style> 
-</head> 
-<body> 
-<p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer imperdiet lectus quis justo...</p><br> 
+/*Реализуйте описанную выше регистрацию. После этого зарегистрируйте нового пользователя и авторизуйтесь под ним. Убедитесь, что все работает, как надо.
+Модифицируйте ваш код так, чтобы кроме логина и пароля пользователю нужно было ввести еще и дату своего рождения и email. Сохраните эти данные в базу данных.*/
+error_reporting(E_ALL); 
+ini_set('display_errors', 'on'); 
 
-<?php 
-if (!empty($_SESSION['auth'])) { 
-echo "<p style=\"color:blue\"> Maecenas aliquet accumsan leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra... </p>"; 
-echo 'Вы зашли на сайт как '.$_SESSION['login'].''; 
-$personal = "<a href=\"personalArea.php\"> Личный кабинет </a>"; 
-$exit = "<a href=\"logout.php\"> Завершить сессию </a>".'<br><br>'; 
-} 
-if (!empty($_SESSION['auth']) && $_SESSION['status'] == 'admin') { ?> 
-<a href="admin.php"> Админка </a><br> 
-<?php } 
+$host = 'localhost'; 
+$user = 'root'; 
+$password = ''; 
+$dbName = 'test'; 
+$link = mysqli_connect($host, $user, $password, $dbName); 
+mysqli_query($link, "SET NAMES 'utf-8'"); 
 
-else { 
-echo "<p><i> Авторизируйтесь, чтобы увидеть продолжение статьи... </i></p> 
-<a href=\"login.php\"> Авторизация </a> &nbsp&nbsp&nbsp <a href=\"register.php\"> Регистрация </a>".'<br><br>'; 
-} 
-echo $_SESSION['result']; 
-unset($_SESSION['result']); 
+// session_start(); 
+
+if(!empty($_POST['log']) and !empty($_POST['pass'])) { 
+$log = $_POST['log']; 
+$pass = $_POST['pass']; 
+$email = $_POST['email']; 
+$birthdate = $_POST['birthdate']; 
+
+$query = "INSERT INTO users (login, password, email, birthday) VALUES ('$log', '$password', '$email', '$birthdate')"; 
+mysqli_query($link, $query) or die(mysqli_error($link)); 
+
+header('Location: /index.php'); 
+} else { 
+
+
 ?> 
-<br> 
-<?= $personal; ?> 
-<br> 
-<?= $exit; ?> 
-</body> 
-</html>
+<form method="POST"> 
+<label for="log">Enter your login:</label><br> 
+<input name="log" id="log"><br><br> 
+<label for="pass">Enter your password:</label><br> 
+<input type="password" name="pass" id="pass"><br><br> 
+<label for="mail">Enter your e-mail:</label><br> 
+<input type="email" name="email"><br><br> 
+<label for="birth"></label><br> 
+<input type="date" name="birthdate"><br><br> 
+<input type="submit"> 
+</form> 
+<?php } ?>
