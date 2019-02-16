737fd2f5a53ec66e2ae35a4804de2076663dc37c
pffsv <pffsv@mail.ru>
2019-02-16 15:35:18 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,6 +1,5 @@
 <?php 
-/*Реализуйте описанную выше регистрацию. После этого зарегистрируйте нового пользователя и авторизуйтесь под ним. Убедитесь, что все работает, как надо.
-Модифицируйте ваш код так, чтобы кроме логина и пароля пользователю нужно было ввести еще и дату своего рождения и email. Сохраните эти данные в базу данных.*/
+//Модифицируйте ваш код так, чтобы после регистрации пользователь автоматически становился авторизованным.
 error_reporting(E_ALL); 
 ini_set('display_errors', 'on'); 
 
@@ -11,17 +10,20 @@ $dbName = 'test';
 $link = mysqli_connect($host, $user, $password, $dbName); 
 mysqli_query($link, "SET NAMES 'utf-8'"); 
 
-// session_start(); 
+session_start(); 
 
 if(!empty($_POST['log']) and !empty($_POST['pass'])) { 
 $log = $_POST['log']; 
 $pass = $_POST['pass']; 
 $email = $_POST['email']; 
 $birthdate = $_POST['birthdate']; 
+$date = date('Y-m-d'); 
 
-$query = "INSERT INTO users (login, password, email, birthday) VALUES ('$log', '$password', '$email', '$birthdate')"; 
+$query = "INSERT INTO users (login, password, email, birthday, registration_date) VALUES ('$log', '$pass', '$email', '$birthdate', '$date')"; 
 mysqli_query($link, $query) or die(mysqli_error($link)); 
 
+$_SESSION['auth'] = true; 
+$_SESSION['logName'] = $log; 
 header('Location: /index.php'); 
 } else { 
 
