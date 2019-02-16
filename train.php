2db8f339bc8d32aeb713627a41d53ba25a9544f2
pffsv <pffsv@mail.ru>
2019-02-16 15:53:51 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,5 +1,4 @@
 <?php 
-//Модифицируйте ваш код так, чтобы после регистрации пользователь автоматически становился авторизованным.
 error_reporting(E_ALL); 
 ini_set('display_errors', 'on'); 
 
@@ -13,31 +12,43 @@ mysqli_query($link, "SET NAMES 'utf-8'");
 session_start(); 
 
 if(!empty($_POST['log']) and !empty($_POST['pass'])) { 
+
 $log = $_POST['log']; 
 $pass = $_POST['pass']; 
 $email = $_POST['email']; 
 $birthdate = $_POST['birthdate']; 
 $date = date('Y-m-d'); 
 
-$query = "INSERT INTO users (login, password, email, birthday, registration_date) VALUES ('$log', '$pass', '$email', '$birthdate', '$date')"; 
+if($_POST['pass'] == $_POST['confirm']) { 
+
+$query = "SELECT * FROM users WHERE login = '$log'"; 
+$user = mysqli_fetch_assoc(mysqli_query($link, $query)); 
+
+if(empty($user)) { 
+
+$query = "INSERT INTO users (login, pass, email, birthdate, registration_date) VALUES ('$log', '$pass', '$email', '$birthdate', '$date')"; 
 mysqli_query($link, $query) or die(mysqli_error($link)); 
 
 $_SESSION['auth'] = true; 
 $_SESSION['logName'] = $log; 
+
+$id = mysqli_insert_id($link); 
+$_SESSION['id'] = $id; 
+
 header('Location: /index.php'); 
 } else { 
+echo 'This login is busy. Try again.'; 
+include 'registerForm.php'; 
+} 
 
 
-?> 
-<form method="POST"> 
-<label for="log">Enter your login:</label><br> 
-<input name="log" id="log"><br><br> 
-<label for="pass">Enter your password:</label><br> 
-<input type="password" name="pass" id="pass"><br><br> 
-<label for="mail">Enter your e-mail:</label><br> 
-<input type="email" name="email"><br><br> 
-<label for="birth"></label><br> 
-<input type="date" name="birthdate"><br><br> 
-<input type="submit"> 
-</form> 
-<?php } ?>
+} else { 
+echo 'Password does not match confirmation. Try again.'; 
+include 'registerForm.php'; 
+} 
+
+} else { 
+
+//include 'registerForm.php'; 
+} 
+?>
