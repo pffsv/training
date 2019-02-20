cd2820fe32f0eab4582a9ca9ee8ff66650ef9968
pffsv <pffsv@mail.ru>
2019-02-20 19:40:51 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,33 +1,17 @@
 <?php
-//Урок 58
-//1.Сделайте класс Employee (работник), в котором будут следующие свойства - name (имя), age (возраст), salary (зарплата).
-class Employee
+class User
 {
-	public $name;
-	public $age;
-	public $salary;
+public $name;
+public $age;
+
+public function show()
+{
+return '!!!';
 }
+}
+$user = new User;
+$user->name = 'Саша';
+$user->age = 25;
 
-//2.Создайте объект класса Employee, затем установите его свойства в следующие значения - имя 'Иван', возраст 25, зарплата 1000.
-
-$employee1 = new Employee;
-$employee1->name = 'Иван';
-$employee1->age = 25;
-$employee1->salary = 1000;
-
-//3.Создайте второй объект класса Employee, установите его свойства в следующие значения - имя 'Вася', возраст 26, зарплата 2000.
-
-$employee2 = new Employee;
-$employee2->name = 'Вася';
-$employee2->age = 26;
-$employee2->salary = 2000;
-
-//4.Выведите на экран сумму зарплат Ивана и Васи.
-
-echo $employee1->salary + $employee2->salary;
-
-//5.Выведите на экран сумму возрастов Ивана и Васи.
-
-echo $employee1->age + $employee2->age;
-
+echo $user->show();
 ?>
