3456d38933081c6650fd2bff975843e4c1ea4504
pffsv <pffsv@mail.ru>
2019-02-21 19:38:59 +0700

    Update post.php

--- post.php
+++ post.php
@@ -1,22 +1,41 @@
 <?php
 
-$name = $_POST['name123'];
+$login = $_POST['login'];
+$password = $_POST['password'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$middle_name = $_POST['middle_name'];
+$email = $_POST['email'];
 
-if (!empty($name)) { // сравниваем $name с '', 0, null
-	echo '<p>Your name is ' . $name . '</p>';
-} else {
-	header('Location: index.php?name_is_empty=1', 301);
+if (empty($login)) { // сравниваем $name с '', 0, null
+	header('Location: index.php?login_is_empty=1', 301);
 }
 
+if (empty($password)) { // сравниваем $name с '', 0, null
+	header('Location: index.php?password_is_empty=1', 301);
+}
+
+if (empty($first_name)) { // сравниваем $name с '', 0, null
+	header('Location: index.php?first_name_is_empty=1', 301);
+}
+
+if (empty($last_name)) { // сравниваем $name с '', 0, null
+    header('Location: index.php?last_name_is_empty=1', 301);
+}
+
+if (empty($middle_name)) { // сравниваем $name с '', 0, null
+}
+
+if (empty($email)) { // сравниваем $name с '', 0, null
+	header('Location: index.php?email_is_empty=1', 301);
+}
+
+echo '<p>Your Login is ' . $login . '</p>';
+echo '<p>Your password is ' . $password . '</p>';
+echo '<p>Your first_name is ' . $first_name . '</p>';
+echo '<p>Your last_name is ' . $last_name . '</p>';
+echo '<p>Your middle_name is ' . $middle_name . '</p>';
+echo '<p>Your email is ' . $email . '</p>';
 ?>
 
 <a href="index.php">Go back</a>
-
-<?php
-
-/**
- * Структура запроса
- * 1. Адрес (URL) 
- * 2. Заголовки запроса
- * 3. Тело запроса
- */
