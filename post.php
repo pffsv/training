82f21b31a933c65fffcfb94fd9b04c65be770e7a
pffsv <pffsv@mail.ru>
2019-02-22 18:09:34 +0700

    Update post.php

--- post.php
+++ post.php
@@ -30,12 +30,33 @@ if (empty($email)) { // сравниваем $name с '', 0, null
 	header('Location: index.php?email_is_empty=1', 301);
 }
 
-echo '<p>Your Login is ' . $login . '</p>';
-echo '<p>Your password is ' . $password . '</p>';
-echo '<p>Your first_name is ' . $first_name . '</p>';
-echo '<p>Your last_name is ' . $last_name . '</p>';
-echo '<p>Your middle_name is ' . $middle_name . '</p>';
-echo '<p>Your email is ' . $email . '</p>';
+// echo '<p>Your Login is ' . $login . '</p>';
+// echo '<p>Your password is ' . $password . '</p>';
+// echo '<p>Your first_name is ' . $first_name . '</p>';
+// echo '<p>Your last_name is ' . $last_name . '</p>';
+// echo '<p>Your middle_name is ' . $middle_name . '</p>';
+// echo '<p>Your email is ' . $email . '</p>';
+
+$user = [
+	'login' => $login,
+	'password' => $password,
+	'first_name' => $first_name,
+	'last_name' => $last_name,
+	'middle_name' => $middle_name,
+	'email' => $email,
+];
+
+foreach ($user as $key => $value) {
+	echo '<p>Your ' . $key . ' is ' . $value . '</p>';
+}
+
+
+// echo '<p>' . $user['password'] . '</p>';
+// echo '<p>' . $user['first_name'] . '</p>';
+// echo '<p>' . $user['last_name'] . '</p>';
+// echo '<p>' . $user['middle_name'] . '</p>';
+// echo '<p>' . $user['email'] . '</p>';
+
 ?>
 
 <a href="index.php">Go back</a>
