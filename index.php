ede9d42b110ab24942e2a1d9c07b1e56cb8b2e81
pffsv <pffsv@mail.ru>
2019-02-22 18:10:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,75 +1,18 @@
+<?php 
 
-<html>
-<head>
-  <meta charset="utf-8"></head>
-  <body>
-    <form action="post.php" method="post">
-      <p>
-        <strong>Login*</strong>       
-        <input name="login">
-      </p>
-      
-      <p>
-        <strong>Password*</strong>    
-        <input type="password" name="password"/>
-      </p>
-      
-      <p>
-        <strong>First name*</strong>   
-        <input type="text" name="first_name" />
-      </p>  
-      
-      <p>
-        <strong>Last name*</strong>    
-        <input type="text" name="last_name" />
-      </p> 
-        
-      <p>
-        <strong>Middle name</strong> 
-        <input type="text" name="middle_name" />
-      </p> 
-
-      <p>
-        <strong>email*</strong>       
-        <input type="text" name="email" />
-      </p>
-
-      
-      <p><input type="submit" /></p>
-  </form>
-<?php
-
-$Login_is_empty = $_GET['login_is_empty'];
-if ($Login_is_empty) {
-  echo '<p style="color: red;">login is required</p>';
+// Определяем что такое машины, что она умеет делать и как.
+class Car
+{
+    var $color = 'White';
+    public $model = 'Honda';
+    function tut_tut()    {  echo "BIp BIp";  }
 }
 
-$Password_is_empty = $_GET['password_is_empty'];
-if ($Password_is_empty) {
-  echo '<p style="color: red;">password is required</p>';
-}
+$honda = new Car(); // присваиваем переменной honda объект - Машину
+//echo '<p>' . $honda->color . '</p>'; // Выводим свойство машины - цвет 
 
-$First_name_is_empty = $_GET['first_name_is_empty'];
-if ($First_name_is_empty) {
-	echo '<p style="color: red;">first_name is required</p>';
-}
+?>
 
-$Last_name_is_empty = $_GET['last_name_is_empty'];
-if ($Last_name_is_empty) {
-	echo '<p style="color: red;">last_name is required</p>';	
-}
-
-$Middle_name_is_empty = $_GET['middle_name_is_empty'];
-if ($Middle_name_is_empty) {
-	echo '<p style="color: red;">middle_name is required</p>';	
-}
-
-$email_is_empty = $_GET['email_is_empty'];
-if ($email_is_empty) {
-	echo '<p style="color: red;">email is required</p>';	
-}
-
-?> 
-
-</body>
-</html>
+<p><?php echo $honda->model ?> is Japan car.<br>Ha har</p>
+<p><?= $honda->model ?> is Japan car.<br>Ha har</p>
+<p>Toyota is another Japan car</p>
