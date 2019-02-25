f2ace27883b7c716cb41cf5932196cb6f829fd21
pffsv <pffsv@mail.ru>
2019-02-25 20:46:37 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,35 +1,17 @@
 <?php
 //62.Модификаторы доступа public и private
-//1.внесите такие же правки в класс User.
-class User
+//2.Попробуйте вызвать метод isAgeCorrect снаружи класса. Убедитесь, что это будет вызывать ошибку.	
+	class User
 	{
-		public $name;
-		public $age;
-		
-		// Метод для изменения возраста юзера:
-		public function setAge($age)
-		{
-			// Проверим возраст на корректность:
-			if ($this->isAgeCorrect($age)) {
-				$this->age = $age;
-			}
-		}
-		
-		// Метод для добавления к возрасту:
-		public function addAge($years)
-		{
-			$newAge = $this->age + $years; // вычислим новый возраст
-			
-			// Проверим возраст на корректность:
-			if ($this->isAgeCorrect($newAge)) {
-				$this->age = $newAge; // обновим, если новый возраст прошел проверку
-			}
-		}
-		
-		// Метод для проверки возраста:
+		private $name;
+		private $age;
+			// Объявим приватным:
 		private function isAgeCorrect($age)
 		{
 			return $age >= 18 and $age <= 60;
-		}
+		}	
+
+	$user = new User;
+    $user->checkAge(30);
 	}
-?>
+?>	
