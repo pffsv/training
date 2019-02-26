4b0caa83566f31fc4e5f23e5ff32faf5ebd194aa
pffsv <pffsv@mail.ru>
2019-02-26 20:38:19 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,6 +1,6 @@
 <?php
 //63.Конструктор объекта
-/*3.Создайте объект класса Employee с именем 'Петя', возрастом 30, зарплатой 2000.*/
+/*4.Выведите на экран сумму зарплат Васи и Пети.*/
 	class Employee
 	{
 		public $name;
@@ -8,7 +8,7 @@
 		public $salary;
 		
 		// Конструктор объекта:
-		public function __construct($name, $age)
+		public function __construct($name, $age, $salary)
 		{
 			$this->name = $name; // запишем данные в свойство name
 			$this->age = $age; // запишем данные в свойство age
@@ -16,6 +16,8 @@
 		}
 	}
 
-	$Employee = new Employee('Петя', 30, 2000); // создадим объект
+	$Employee1 = new Employee('Вася', 25, 1000);
+	$Employee2 = new Employee('Петя', 30, 2000);
+	echo $Employee1->salary + $Employee2->salary;
 	
 ?>	
