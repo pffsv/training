a2bf0526f0350ff232d41786908474b32ee7caaf
pffsv <pffsv@mail.ru>
2019-02-27 20:46:22 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,45 +1,46 @@
 <?php
 //64.Работа с геттерами и сеттерами
-/*3.Дополните класс Employee приватным методом isAgeCorrect, 
-который будет проверять возраст на корректность (от 1 до 100 лет). 
-Этот метод должен использоваться в сеттере setAge перед установкой 
-нового возраста (если возраст не корректный - он не должен меняться).*/
-	class Employee
-	{
-		public $name;
-		public $age;
-		public $salary;
+/*4.Пусть зарплата наших работников хранится в долларах. Сделайте так, чтобы геттер getSalary добавлял в конец числа с зарплатой значок доллара.*/
+class Employee
+{
+private $name;
+private $age;
+private $salary;
 
-	public function setName($name)
-	{
-		$this->name = $name;
-	}
-	public function getName()
-	{
-		return $this->name;
-	}
-	public function setAge($age)
-	{
-		if ($this->isAgeCorrect($age)) {
-		$this->age = $age;
-	}
-    }
-	public function getAge()
-	{
-		return $this->age;
-	}
-	public function setSalary($salary)
-	{
-		$this->salary = $salary;
-	}
-	public function getSalary()
-	{
-		return $this->salary;
-	}
-	private function isAgeCorrect($age)
-	{
-	return $age >= 1 and $age <= 100;
-	}	
-	}
+public function setName($name)
+{
+$this->name = $name;
+}
+public function getName()
+{
+return $this->name;
+}
+public function setAge($age)
+{
+if ($this->isAgeCorrect($age)) {
+$this->age = $age;
+}
+}
+public function getAge()
+{
+return $this->age;
+}
+public function setSalary($salary)
+{
+$this->salary = $salary;
+}
+public function getSalary()
+{
+return $this->salary.'$';
+}
+private function isAgeCorrect($age)
+{
+return $age >= 1 and $age <= 100;
+}
+}
+$emp = new Employee;
+
+$emp->setSalary(500);
+echo $emp->getSalary();// 500$
 	
 ?>	
