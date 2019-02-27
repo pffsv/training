9da70bf76e11e4884eb8b4438e8206fb6358f4a3
pffsv <pffsv@mail.ru>
2019-02-27 20:41:41 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,12 +1,38 @@
 <?php
 //64.Работа с геттерами и сеттерами
-/*1.Сделайте класс Employee, в котором будут следующие private свойства: name (имя), age (возраст) и salary (зарплата).*/
+/*2.Сделайте геттеры и сеттеры для всех свойств класса Employee*/
 	class Employee
 	{
 		public $name;
 		public $age;
 		public $salary;
-		
+
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
+	public function getName()
+	{
+		return $this->name;
+	}
+	public function setAge($age)
+	{
+		if ($this->isAgeCorrect($age)) {
+		$this->age = $age;
+	}
+    }
+	public function getAge()
+	{
+		return $this->age;
+	}
+	public function setSalary($salary)
+	{
+		$this->salary = $salary;
+	}
+	public function getSalary()
+	{
+		return $this->salary;
+	}	
 	}
 	
 ?>	
