b5a0e1a011f6ab58a1c824887e27b79a8670f3ae
pffsv <pffsv@mail.ru>
2019-02-27 20:36:35 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,23 +1,12 @@
 <?php
-//63.Конструктор объекта
-/*4.Выведите на экран сумму зарплат Васи и Пети.*/
+//64.Работа с геттерами и сеттерами
+/*1.Сделайте класс Employee, в котором будут следующие private свойства: name (имя), age (возраст) и salary (зарплата).*/
 	class Employee
 	{
 		public $name;
 		public $age;
 		public $salary;
 		
-		// Конструктор объекта:
-		public function __construct($name, $age, $salary)
-		{
-			$this->name = $name; // запишем данные в свойство name
-			$this->age = $age; // запишем данные в свойство age
-			$this->salary = $salary; // запишем данные в свойство salary
-		}
 	}
-
-	$Employee1 = new Employee('Вася', 25, 1000);
-	$Employee2 = new Employee('Петя', 30, 2000);
-	echo $Employee1->salary + $Employee2->salary;
 	
 ?>	
