7279f318da080e3041d2543ebe6a0bd13db900b7
pffsv <pffsv@mail.ru>
2019-02-28 20:28:38 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,46 +1,11 @@
 <?php
-//64.Работа с геттерами и сеттерами
-/*4.Пусть зарплата наших работников хранится в долларах. Сделайте так, чтобы геттер getSalary добавлял в конец числа с зарплатой значок доллара.*/
+//65.Свойства только для чтения
+/*1.Сделайте класс Employee, в котором будут следующие свойства: name (имя), surname (фамилия) и salary (зарплата).*/
 class Employee
 {
 private $name;
 private $age;
 private $salary;
+}
 
-public function setName($name)
-{
-$this->name = $name;
-}
-public function getName()
-{
-return $this->name;
-}
-public function setAge($age)
-{
-if ($this->isAgeCorrect($age)) {
-$this->age = $age;
-}
-}
-public function getAge()
-{
-return $this->age;
-}
-public function setSalary($salary)
-{
-$this->salary = $salary;
-}
-public function getSalary()
-{
-return $this->salary.'$';
-}
-private function isAgeCorrect($age)
-{
-return $age >= 1 and $age <= 100;
-}
-}
-$emp = new Employee;
-
-$emp->setSalary(500);
-echo $emp->getSalary();// 500$
-	
 ?>	
