d7a118603cefa0355c66634b9972e608ce20e208
pffsv <pffsv@mail.ru>
2019-03-02 14:41:21 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,40 +1,28 @@
 <?php
-//66.Хранение классов в отдельных файлах
-/*1.Сделайте класс Employee, в котором будут следующие свойства: name (имя), surname (фамилия), patronymic (отчество) и salary (зарплата).
-Пусть этот класс хранится в отдельном файле.*/
-class Employee
+//67.Хранение объектов в массивах
+/*1.Сделайте класс City (город), в котором будут следующие свойства: name (название города), population (количество населения).*/
+class City
 {
-private $name;
-private $surname;
-private $patronymic;
-private $salary;
+	public $name;
+	public $population;
 
-function __construct($name, $patronymic, $surname, $salary)
-{
-$this->name = $name;
-$this->surname = $surname;
-$this->salary = $salary;
+	public function __construct($name, $population)
+	{
+		$this->name = $name;
+		$this->population = $population;
+	}
 }
-public function getName()
-{
-return $this->name;
-}
-public function getSurname()
-{
-return $this->surname;
-}
-public function getPatronymic()
-{
-return $this->patronymic;
-}
-public function getSalary()
-{
-return $this->salary.'$';
-}
-public function setSalary($salary)
-{
-$this->salary = $salary;
-}
-}
+//2.Создайте 5 объектов класса City, заполните их данными и запишите в массив.
+	$cities = [
+		new City('Москва', 80000000),
+		new City('Новосибирск', 1000000),
+		new City('Томск', 500000),
+		new City('Кемерово', 400000),
+		new City('Омск', 1000000)
+		];
 
+//3.Переберите созданный вами массив с городами циклом и выведите города и их население на экран.
+	foreach ($cities as $city) {
+		echo 'г. '.$city->name.' (кол-во населения: '.$city->population.')<br>';
+	}		
 ?>	
