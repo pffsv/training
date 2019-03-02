74782df99cecb22609153dfa432cfa99564c3560
pffsv <pffsv@mail.ru>
2019-03-02 15:01:05 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,49 +1,24 @@
 <?php
-//68.Начальные значения свойств в конструкторе
-/*1.реализуйте такой же класс Student.*/
-/*2.Модифицируйте метод transferToNextCourse так, чтобы при переводе на новый курс выполнялась проверка того, что новый курс не будет больше 5.*/
-class Student
+//69.Начальные значения свойств при объявлении
+/*1.Реализуйте класс Arr, похожий на тот, который я реализовал выше.
+В отличие от моего класса метод add вашего класса параметром должен принимать массив чисел. 
+Все числа из этого массива должны добавляться в конец массива $this->numbers.*/
+/*2.Вместо метода getSum реализуйте метод getAvg, который будет находить среднее арифметическое переданных чисел.*/
+class Arr
 {
-	private $name;
-	private $course;
+private $numbers = [];
 
-	function __construct($name)
-	{
-		$this->name = $name;
-		$this->course = 1;
-	}
-	public function getName()
-	{
-	return $this->name;
-	}
-	public function getCourse()
-	{
-	return $this->course;
-	}
-
-	public function transferToNextCourse()
-	{
-	$newcourse = $this->course + 1;
-	if ($this->isCourseCorrect($newcourse)) {
-	$this->course = $newcourse;
-	}
-	}
-	private function isCourseCorrect($newcourse)
-	{
-	return $newcourse <= 5;
-	}
+public function add($arr)
+{
+$this->numbers = array_merge($this->numbers, $arr);
 }
-
-	$std = new Student('Вася');
-	echo $std->getCourse();// 1
-	$std->transferToNextCourse();
-	echo $std->getCourse();// 2
-	$std->transferToNextCourse();
-	echo $std->getCourse();// 3
-	$std->transferToNextCourse();
-	echo $std->getCourse();// 4
-	$std->transferToNextCourse();
-	echo $std->getCourse();// 5
-	$std->transferToNextCourse();
-	echo $std->getCourse();// 5	
+public function getAvg()
+{
+return array_sum($this->numbers) / count($this->numbers);
+}
+}
+$array = [1, 2, 3, 4, 5];
+$arr = new Arr;
+$arr->add($array);
+echo $arr->getAvg();// 3
 ?>	
