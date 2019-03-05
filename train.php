b646c81232d947549f77ae79d50a08e66ffe25b5
pffsv <pffsv@mail.ru>
2019-03-05 20:26:43 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,25 +1,32 @@
 <?php
-//70.Переменные названия свойств
-/*1.Скопируйте мой код класса User и массив $props. В моем примере на экран выводится фамилия юзера. Выведите еще и имя, и отчество.*/
+//71.Переменные названия методов
+/*1.Пусть массив $methods будет ассоциативным с ключами method1 и method2:
+
+	$methods = ['method1' => 'getName', 'method2' => 'getAge'];
+
+Выведите имя и возраст пользователя с помощью этого массива.*/
 class User
 {
-public $surname; // фамилия
-public $name; // имя
-public $patronymic; // отчество
+private $name;
+private $age;
 
-public function __construct($surname, $name, $patronymic)
+public function __construct($name, $age)
 {
-$this->surname = $surname;
 $this->name = $name;
-$this->patronymic = $patronymic;
+$this->age = $age;
 }
-}
-$user = new User('Иванов', 'Иван', 'Иванович');
-$props = ['surname', 'name', 'pat' => 'patronymic'];
-function getProp()
-{
-return 'name';
-}
-echo $user->{$props[0]}.' '.$user->{getProp()}.' '.$user->{$props['pat']};
 
+public function getName()
+{
+return $this->name;
+}
+
+public function getAge()
+{
+return $this->age;
+}
+}
+$user = new User('Коля', 21);
+$methods = ['method1' => 'getName', 'method2' => 'getAge'];
+echo $user->{$methods['method1']}() . ' ' . $user->{$methods['method2']}();
 ?>	
