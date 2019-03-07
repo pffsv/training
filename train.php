fdbc34a2f138e4f2841407f61d7bcd9519ee954d
pffsv <pffsv@mail.ru>
2019-03-07 17:58:27 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,66 +1,47 @@
 <?php
-////73.Цепочки методов
-/*1.Не подсматривая в мой код самостоятельно реализуйте такой же класс Arr, методы которого будут вызываться в виде цепочки.*/
-/*2.Добавьте в класс Arr еще один метод append, который параметром будет принимать массив чисел и добавлять эти числа в конец массива, хранящегося в объекте.
-Предполагается, что методы add и append можно будет использовать в любом порядке:
-	echo (new Arr)->add(1)->append([2, 3, 4])->add(5)->getSum();.*/
-
-class Arr
+//74.Класс как набор методов
+/*1Напишите реализацию методов класса ArrayAvgHelper*/
+class ArraySumHelper
 {
-private $numbers = [];
-
-public function add($number)
+public function getAvg1($arr)
 {
-$this->numbers[] = $number;
-return $this;
+return $this->getSum($arr, 1);
 }
 
-public function append($arr)
+public function getAvg2($arr)
 {
-$this->numbers = array_merge($this->numbers, $arr);
-return $this;
+return $this->calcSqrt($this->getSum($arr, 2), 2);
 }
-public function getSum()
-{
-return array_sum($this->numbers);
-}
-}
-$array = [2, 3, 4];
-echo (new Arr)->add(1)->add(3)->add(5)->getSum();// 9
-echo (new Arr)->add(10)->append($array)->add(5)->getSum();// 24
 
-/*Сделайте класс User, у которого будут приватные свойства surname (фамилия), name (имя) и patronymic (отчество).
-Эти свойства должны задаваться с помощью соответствующих сеттеров.
-Сделайте так, чтобы эти сеттеры вызывались цепочкой в любом порядке, 
-а самым последним методом в цепочке можно было вызвать метод getFullName, который вернет ФИО юзера (первую букву фамилии, имени и отчества).
-Пример:
-	echo (new User)->setName('Николай')->setPatronymic('Иванович')
-		->setSurname('Петров')->getFullName(); // выведет 'ПНИ'*/
-class User
+public function getAvg3($arr)
 {
-private $name;
-private $surname;
-private $patronymic;
+return $this->calcSqrt($this->getSum($arr, 3), 3);
+}
 
-public function setName($name)
+public function getAvg4($arr)
 {
-$this->name = $name;
-return $this;
+return $this->calcSqrt($this->getSum($arr, 4), 4);
 }
-public function setSurname($surname)
+
+private function getSum($arr, $power)
 {
-$this->surname = $surname;
-return $this;
+$sum = 0;
+foreach ($arr as $elem) {
+$sum += pow($elem, $power);
 }
-public function setPatronymic($patronymic)
+return $sum;
+}
+
+private function calcSqrt($num, $power)
 {
-$this->patronymic = $patronymic;
-return $this;
-}
-public function getFullName()
-{
-return mb_substr($this->surname, 0, 1).mb_substr($this->name, 0, 1).mb_substr($this->patronymic, 0, 1);
+return pow($num, 1 / $power);
 }
 }
-echo (new User)->setName('Николай')->setPatronymic('Иванович')->setSurname('Петров')->getFullName();
+$arr = [1, 2, 3];
+
+echo (new ArraySumHelper)->getAvg1($arr);
+echo (new ArraySumHelper)->getAvg2($arr);
+echo (new ArraySumHelper)->getAvg3($arr);
+echo (new ArraySumHelper)->getAvg4($arr);
+
 ?>	
