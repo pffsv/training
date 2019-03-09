daa30f75ec5eb9eab8a5233584f090fbe5d70d1e
pffsv <pffsv@mail.ru>
2019-03-09 09:26:20 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,10 +1,16 @@
 <?php
-//76.Модификатор доступа protected
-/*1.реализуйте описанный класс Student с методами getCourse, setCourse и addOneYear.*/
+//77.Перезапись методов родителя в классе потомке
+/*1.Модифицируйте код класса User так, чтобы в методе setName
+выполнялась проверка того, что длина имени более 3-х символов.
+2.Добавьте в класс Student метод setName, в котором будет
+выполняться проверка того, что длина имени более 3-х символов
+и менее 10 символов.
+Пусть метод setName класса Student использует метод setName
+своего родителя, чтобы выполнить часть проверки.
+*/
 class User
 {
 private $name;
-protected $age;
 
 public function getName()
 {
@@ -13,40 +19,32 @@ return $this->name;
 
 public function setName($name)
 {
+if (mb_strlen($name) > 3) {
 $this->name = $name;
 }
 
-public function getAge()
-{
-return $this->age;
-}
-
-public function setAge($age)
-{
-$this->age = $age;
 }
 }
 class Student extends User
 {
-private $course;
+public function setName($name)
+{
+if (mb_strlen($name) < 10) {
+parent::setName($name);
+}
+}
+}
+$us = new User;
+$us->setName('Вася');
+echo $us->getName();//Вася
+$us->setName('Ая');
+echo $us->getName();//Вася
 
-public function addOneYear()
-{
-$this->age++;
-}
-public function getCourse()
-{
-return $this->course;
-}
-public function setCourse($course)
-{
-$this->course = $course;
-}
-}
 $std = new Student;
-$std->setAge(30);
-echo $std->getAge();// 30
-$std->addOneYear();
-echo $std->getAge();// 31
-//echo $std->age;// err
+$std->setName('Саня');
+echo $std->getName();//Саня
+$std->setName('Ая');
+echo $std->getName();//Саня
+$std->setName('Константин');//10 символов
+echo $std->getName();//Саня
 ?>	
