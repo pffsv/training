75e4895fcbef51e6a0295ff06072203a0c38c62f
pffsv <pffsv@mail.ru>
2019-03-09 09:33:41 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,50 +1,41 @@
 <?php
-//77.Перезапись методов родителя в классе потомке
-/*1.Модифицируйте код класса User так, чтобы в методе setName
-выполнялась проверка того, что длина имени более 3-х символов.
-2.Добавьте в класс Student метод setName, в котором будет
-выполняться проверка того, что длина имени более 3-х символов
-и менее 10 символов.
-Пусть метод setName класса Student использует метод setName
-своего родителя, чтобы выполнить часть проверки.
-*/
+//78.Перезапись конструктора родителя в потомке
+/*1.реализуйте такой же класс Student, наследующий от User.*/
 class User
 {
 private $name;
+private $age;
 
+public function __construct($name, $age)
+{
+$this->name = $name;
+$this->age = $age;
+}
 public function getName()
 {
 return $this->name;
 }
-
-public function setName($name)
+public function getAge()
 {
-if (mb_strlen($name) > 3) {
-$this->name = $name;
-}
-
+return $this->age;
 }
 }
 class Student extends User
 {
-public function setName($name)
-{
-if (mb_strlen($name) < 10) {
-parent::setName($name);
-}
-}
-}
-$us = new User;
-$us->setName('Вася');
-echo $us->getName();//Вася
-$us->setName('Ая');
-echo $us->getName();//Вася
+private $course;
 
-$std = new Student;
-$std->setName('Саня');
-echo $std->getName();//Саня
-$std->setName('Ая');
-echo $std->getName();//Саня
-$std->setName('Константин');//10 символов
-echo $std->getName();//Саня
+function __construct($name, $age, $course)
+{
+parent::__construct($name, $age);
+$this->course = $course;
+}
+public function getCourse()
+{
+return $this->course;
+}
+}
+$std = new Student('Вася', 21, 1);
+echo $std->getName();//Вася
+echo $std->getAge();// 21
+echo $std->getCourse();// 1
 ?>	
