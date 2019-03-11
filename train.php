959e61e70de4a45002b24251b269406a0a702f38
pffsv <pffsv@mail.ru>
2019-03-11 16:52:47 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,63 +1,28 @@
 <?php
-//78.Перезапись конструктора родителя в потомке
-/*6.Сделайте класс Employee, который будет наследовать от класса User. 
-Пусть новый класс имеет свойство salary, в котором будет хранится зарплата работника. 
-Зарплата должна передаваться четвертым параметром в конструктор объекта. Сделайте также геттер для этого свойства.*/
-
-class User
+//79.Передача объектов по ссылке
+/*1.Сделайте класс Product (продукт),
+в котором будут следующие свойства:
+name (название продукта), price (цена).
+2.Создайте объект класса Product, запишите
+его в переменную $product1.
+3.Присвойте значение переменной $product1
+в переменную $product2. Проверьте то, что
+обе переменные ссылаются на один и тот же объект.
+*/
+class Product
 {
-private $name;
-private $surname;
-private $birthday;
-private $age;
+public $name;
+public $price;
 
-public function __construct($name, $surname, $birthday)
+function __construct($name, $price)
 {
 $this->name = $name;
-$this->surname = $surname;
-$this->birthday = $birthday;
-$this->age = $this->calculateAge($birthday);
-}
-public function getName()
-{
-return $this->name;
-}
-public function getSurname()
-{
-return $this->surname;
-}
-public function getBirthday()
-{
-return $this->birthday;
-}
-public function getAge()
-{
-return $this->age;
-}
-private function calculateAge($birthday)
-{
-return (new DateTime())->diff(new DateTime($birthday))->format('%Y');
+$this->price = $price;
 }
 }
-$us = new User('Вася', 'Пупкин', '1985-02-10');
-echo $us->getAge();// 33
-
-class Employee extends User
-{
-private $salary;
-
-function __construct($name, $surname, $birthday, $salary)
-{
-parent::__construct($name, $surname, $birthday);
-$this->salary = $salary;
-}
-public function getSalary()
-{
-return $this->salary;
-}
-}
-$emp = new Employee('Вася', 'Пупкин', '1985-02-10', 1500);
-echo $emp->getAge();// 33
-echo $emp->getSalary();// 1500
+$product1 = new Product('чапельник', 1000);
+echo $product1->name;// чапельник
+$product2 = $product1;
+echo $product2->name;// чапельник
 
 ?>	
