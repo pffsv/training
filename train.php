9de88bc334bbd473f2fcdd8dd25383a4f58c0961
pffsv <pffsv@mail.ru>
2019-03-12 14:32:20 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,28 +1,95 @@
 <?php
-//79.Передача объектов по ссылке
-/*1.Сделайте класс Product (продукт),
-в котором будут следующие свойства:
-name (название продукта), price (цена).
-2.Создайте объект класса Product, запишите
-его в переменную $product1.
-3.Присвойте значение переменной $product1
-в переменную $product2. Проверьте то, что
-обе переменные ссылаются на один и тот же объект.
+//80.Использование классов внутри других классов
+/*
+1.повторите описанные мною классы Arr и SumHelper.
+2.Создайте класс AvgHelper с методом getAvg,
+который параметром будет принимать массив и
+возвращать среднее арифметическое этого массива
+(сумма элементов делить на количество).
+3.Добавьте в класс AvgHelper еще и метод getMeanSquare,
+который параметром будет принимать массив и возвращать
+среднее квадратичное этого массива (квадратный корень,
+извлеченный из суммы квадратов элементов, деленной на количество).
+4.Добавьте в класс Arr метод getAvgMeanSum, который
+будет находить сумму среднего арифметического
+и среднего квадратичного из массива $this->nums.
 */
-class Product
+class Arr
 {
-public $name;
-public $price;
+private $nums = [];
+private $sumHelper;
+private $avgHelper;
 
-function __construct($name, $price)
+public function __construct()
 {
-$this->name = $name;
-$this->price = $price;
+$this->sumHelper = new SumHelper;
+$this->avgHelper = new AvgHelper;
+}
+
+public function getAvgMeanSum()
+{
+return $this->avgHelper->getAvg($this->nums) + $this->avgHelper->getMeanSquare($this->nums);
+}
+
+public function getSum23()
+{
+return $this->sumHelper->getSum2($this->nums) + $this->sumHelper->getSum3($this->nums);
+}
+
+public function add($number)
+{
+$this->nums[] = $number;
+return $this;
 }
 }
-$product1 = new Product('чапельник', 1000);
-echo $product1->name;// чапельник
-$product2 = $product1;
-echo $product2->name;// чапельник
+class SumHelper
+{
+public function getSum1($arr)
+{
+return $this->getSum($arr, 1);
+}
+public function getSum2($arr)
+{
+return $this->getSum($arr, 2);
+}
 
+public function getSum3($arr)
+{
+return $this->getSum($arr, 3);
+}
+
+private function getSum($arr, $power) {
+$sum = 0;
+
+foreach ($arr as $elem) {
+$sum += pow($elem, $power);
+}
+
+return $sum;
+}
+}
+
+class AvgHelper
+{
+private $sumHelper;
+
+function __construct()
+{
+$this->sumHelper = new SumHelper;
+}
+public function getAvg($arr)
+{
+return $this->sumHelper->getSum1($arr) / count($arr);
+}
+public function getMeanSquare($arr)
+{
+return $this->getSquare2($this->sumHelper->getSum2($arr) / count($arr));
+}
+private function getSquare2($num)
+{
+return pow($num, 1/2);
+}
+}
+$arr = (new Arr)->add(2)->add(4);
+echo $arr->getAvgMeanSum();// 6.1622
 ?>	
