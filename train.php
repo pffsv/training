0d764e1eb21c4b397e4ef18f2d4d335dce11185c
pffsv <pffsv@mail.ru>
2019-03-14 16:07:45 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,107 +1,117 @@
 <?php
-//81.Передача объектов параметрами
+//82.Сравнение объектов
 /*
-1.Сделайте класс Product (товар), в котором будут
-приватные свойства name (название товара), price
-(цена за штуку) и quantity. Пусть все эти свойства
-будут доступны только для чтения.
-2.Добавьте в класс Product метод getCost, который
-будет находить полную стоимость продукта (сумма
-умножить на количество).
-3.Сделайте класс Cart (корзина). Данный класс
-будет хранить список продуктов (объектов класса
-Product) в виде массива. Пусть продукты хранятся
-в свойстве products.
-4.Реализуйте в классе Cart метод add для добавления продуктов.
-5.Реализуйте в классе Cart метод remove для удаления продуктов.
-Метод должен принимать параметром название удаляемого продукта.
-6.Реализуйте в классе Cart метод getTotalCost, который будет
-находить суммарную стоимость продуктов.
-7. Реализуйте в классе Cart метод getTotalQuantity,
-который будет находить суммарное количество продуктов
-(то есть сумму свойств quantity всех продуктов).
-8.Реализуйте в классе Cart метод getAvgPrice,
-который будет находить среднюю стоимость продуктов
-(суммарная стоимость делить на количество всех продуктов).
+1.Сделайте функцию compare, которая параметром будет
+принимать два объекта и возвращать true, если они имеют
+одинаковые свойства и значения являются экземплярами
+одного и того же класса, и false, если это не так.
+2.Сделайте функцию compare, которая параметром будет
+принимать два объекта и возвращать true, если
+переданные переменные ссылаются на один и тот
+же объект, и false, если на разные.
+3.Сделайте функцию compare, которая параметром
+будет принимать два объекта и сравнивать их.
+Функция должна возвращать 1, если переданные
+переменные ссылаются на один и тот же объект.
+Функция должна возвращать 0, если объекты разные, но
+одного и того же класса и с теми же свойствами и их значениями.
+Функция должна возвращать -1 в противном случае.
+4.Скопируйте мой код класса Employee, затем самостоятельно
+реализуйте описанный класс EmployeesCollection, проверьте его работу.
+5.Упростите ваш класс EmployeesCollection с
+использованием функции in_array, проверьте его работу.
 */
-class Product
+class User
 {
 private $name;
-private $price;
-private $quantity;
+private $age;
 
-function __construct($name, $price, $quantity)
+public function __construct($name, $age)
 {
 $this->name = $name;
-$this->price = $price;
-$this->quantity = $quantity;
+$this->age = $age;
 }
+}
+
+class Compare
+{
+public static function obj($obj1, $obj2)
+{
+return $obj1 == $obj2;
+}
+}
+class Compare1
+{
+public static function obj($obj1, $obj2)
+{
+return $obj1 === $obj2;
+}
+}
+
+class Compare2
+{
+public static function obj($obj1, $obj2)
+{
+if ($obj1 === $obj2) {
+return 1;
+} elseif ($obj1 == $obj2) {
+return 0;
+} else {
+return -1;
+}
+}
+}
+$us1 = new User('Вася', 30);
+$us2 = new User('Вася', '30');
+$us3 = new User('Вова', 25);
+$us4 = $us1;
+
+var_dump(Compare::obj($us1, $us2));// true
+var_dump (Compare1::obj($us1, $us2));// false
+echo Compare2::obj($us1, $us4);// 1
+echo Compare2::obj($us1, $us2);// 0
+echo Compare2::obj($us1, $us3);// -1
+
+class Employee
+{
+private $name;
+private $salary;
+
+public function __construct($name, $salary)
+{
+$this->name = $name;
+$this->salary = $salary;
+}
+
 public function getName()
 {
 return $this->name;
 }
-public function getPrice()
-{
-return $this->price;
-}
-public function getQuantity()
-{
-return $this->quantity;
-}
-public function getCost()
-{
-return $this->price * $this->quantity;
-}
-}
 
-class Cart
+public function getSalary()
 {
-private $products = [];
+return $this->salary;
+}
+}
+class EmployeesCollection
+{
+private $employees = [];
+
+public function add($newEmployee)
+{
+if (!in_array($newEmployee, $this->employees, true)) {
+$this->employees[] = $newEmployee;
+}
+}
 
 public function get()
 {
-return $this->products;
-}
-public function add($products)
-{
-$this->products[] = $products;
-return $this;
-}
-public function remove($name)
-{
-foreach ($this->products as $key=>$value){
-if ($value->getName() == $name) {
-unset($this->products[$key]);
+return $this->employees;
 }
 }
-}
-public function getTotalCost()
-{
-$cost = 0;
-foreach ($this->products as $products) {
-$cost += $products->getCost();
-}
-return $cost;
-}
-public function getTotalQuantity()
-{
-$qnt = 0;
-foreach ($this->products as $products) {
-$qnt += $products->getQuantity();
-}
-return $qnt;
-}
-public function getAvgPrice()
-{
-return $this->getTotalCost() / $this->getTotalQuantity();
-}
-}
-$cart = (new Cart)->add(new Product('чапельник', 500, 4))->add(new Product('сковорода', 1000, 4));
-
-var_dump($cart->get());//['чапельник', 500, 4], ['сковорода', 1000, 4]
-echo $cart->getTotalCost();// 6000
-echo $cart->getTotalQuantity();// 8
-echo $cart->getAvgPrice();// 750
-$cart->remove('чапельник');// ['сковорода', 1000, 4]
-var_dump($cart->get());
+$empCol = new EmployeesCollection;
+$employee = new Employee('Коля', 100);
+$empCol->add($employee);
+$empCol->add($employee);
+var_dump($empCol->get());// 'Коля', 100
 ?>	
