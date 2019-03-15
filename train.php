64869c2b44cdcb55adf3e7914f4a0057f5edd3a2
pffsv <pffsv@mail.ru>
2019-03-15 14:15:58 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,117 +1,70 @@
 <?php
-//82.Сравнение объектов
+//83.Определение принадлежности объекта к классу
 /*
-1.Сделайте функцию compare, которая параметром будет
-принимать два объекта и возвращать true, если они имеют
-одинаковые свойства и значения являются экземплярами
-одного и того же класса, и false, если это не так.
-2.Сделайте функцию compare, которая параметром будет
-принимать два объекта и возвращать true, если
-переданные переменные ссылаются на один и тот
-же объект, и false, если на разные.
-3.Сделайте функцию compare, которая параметром
-будет принимать два объекта и сравнивать их.
-Функция должна возвращать 1, если переданные
-переменные ссылаются на один и тот же объект.
-Функция должна возвращать 0, если объекты разные, но
-одного и того же класса и с теми же свойствами и их значениями.
-Функция должна возвращать -1 в противном случае.
-4.Скопируйте мой код класса Employee, затем самостоятельно
-реализуйте описанный класс EmployeesCollection, проверьте его работу.
-5.Упростите ваш класс EmployeesCollection с
-использованием функции in_array, проверьте его работу.
+Определение принадлежности объекта к классу
+
+1.Сделайте класс Employee с публичными
+свойствами name (имя) и salary (зарплата).
+2.Сделайте класс Student с публичными свойствами
+name (имя) и scholarship (стипендия).
+3.Создайте по 3 объекта каждого класса и в
+произвольном порядке запишите их в массив $arr.
+4.Переберите циклом массив $arr и выведите на
+экран столбец имен всех работников.
+5.Аналогичным образом выведите на экран
+столбец имен всех студентов.
+6.Переберите циклом массив $arr и с его помощью
+найдите сумму зарплат работников и сумму стипендий
+студентов. После цикла выведите эти два числа на экран.
 */
-class User
-{
-private $name;
-private $age;
-
-public function __construct($name, $age)
-{
-$this->name = $name;
-$this->age = $age;
-}
-}
-
-class Compare
-{
-public static function obj($obj1, $obj2)
-{
-return $obj1 == $obj2;
-}
-}
-class Compare1
-{
-public static function obj($obj1, $obj2)
-{
-return $obj1 === $obj2;
-}
-}
-
-class Compare2
-{
-public static function obj($obj1, $obj2)
-{
-if ($obj1 === $obj2) {
-return 1;
-} elseif ($obj1 == $obj2) {
-return 0;
-} else {
-return -1;
-}
-}
-}
-$us1 = new User('Вася', 30);
-$us2 = new User('Вася', '30');
-$us3 = new User('Вова', 25);
-$us4 = $us1;
-
-var_dump(Compare::obj($us1, $us2));// true
-var_dump (Compare1::obj($us1, $us2));// false
-echo Compare2::obj($us1, $us4);// 1
-echo Compare2::obj($us1, $us2);// 0
-echo Compare2::obj($us1, $us3);// -1
-
 class Employee
 {
-private $name;
-private $salary;
+public $name;
+public $salary;
 
-public function __construct($name, $salary)
+function __construct($name, $salary)
 {
 $this->name = $name;
 $this->salary = $salary;
 }
-
-public function getName()
-{
-return $this->name;
 }
+class Student
+{
+public $name;
+public $scholarship;
 
-public function getSalary()
+function __construct($name, $scholarship)
 {
-return $this->salary;
-}
-}
-class EmployeesCollection
-{
-private $employees = [];
-
-public function add($newEmployee)
-{
-if (!in_array($newEmployee, $this->employees, true)) {
-$this->employees[] = $newEmployee;
+$this->name = $name;
+$this->scholarship = $scholarship;
 }
 }
 
-public function get()
-{
-return $this->employees;
+$arr[] = new Employee('Вася', 2500);
+$arr[] = new Employee('Коля', 2000);
+$arr[] = new Employee('Саша', 3000);
+$arr[] = new Student('Женя', 1000);
+$arr[] = new Student('Олег', 900);
+$arr[] = new Student('Дима', 950);
+foreach ($arr as $name) {
+if ($name instanceof Employee) {
+echo $name->name;// ВасяКоляСаша
 }
 }
-$empCol = new EmployeesCollection;
-$employee = new Employee('Коля', 100);
-$empCol->add($employee);
-$empCol->add($employee);
-var_dump($empCol->get());// 'Коля', 100
+foreach ($arr as $name) {
+if ($name instanceof Student) {
+echo $name->name;// ЖеняОлегДима
+}
+}
+$sal = 0;
+$sch = 0;
+foreach ($arr as $sum) {
+if ($sum instanceof Employee) {
+$sal += $sum->salary;
+} elseif ($sum instanceof Student) {
+$sch += $sum->scholarship;
+}
+}
+echo 'Сумма ЗП: '.$sal.', Сумма стипендий: '.$sch;
+
 ?>	
