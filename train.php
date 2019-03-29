5168b143848241eca0bf02085a42de58102fbf47
pffsv <pffsv@mail.ru>
2019-03-30 06:16:40 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,61 +1,106 @@
 <?php
 /*
-94.Наследование интерфейсов друг от друга
+95.Интерфейсы и instanceof
 
-1.Сделайте интерфейс iUser с методами
-getName, setName, getAge, setAge.
-2.Сделайте интерфейс iEmployee,
-наследующий от интерфейса iUser и добавляющий
-в него методы getSalary и setSalary.
-3.Сделайте класс Employee, реализующий интерфейс iEmployee.
+1.Сделайте интерфейс Figure3d (трехмерная фигура),
+который будет иметь метод getVolume (получить объем) и
+метод getSurfaceSquare (получить площадь поверхности).
+2.Сделайте класс Cube (куб), который будет
+реализовывать интерфейс Figure3d.
+3.Создайте несколько объектов класса Quadrate,
+несколько объектов класса Rectangle и несколько
+объектов класса Cube. Запишите их в массив $arr
+в случайном порядке.
+4.Переберите циклом массив $arr и выведите на
+экран только площади объектов реализующих интерфейс Figure.
+5.Переберите циклом массив $arr и выведите для плоских
+фигур их площади, а для объемных - площади их поверхности.
 */
-interface iUser
+interface Figure
 {
-public function setName($name);
-public function getName();
-public function setAge($age);
-public function getAge();
+public function getSquare();
+public function getPerimeter();
 }
-interface iEmployee
+interface Figure3d
 {
-public function setSalary($salary);
-public function getSalary();
+public function getVolume();
+public function getSurfaceSquare();
+}
+class Quadrate implements Figure
+{
+private $a;
+
+public function __construct($a)
+{
+$this->a = $a;
+}
+public function getSquare()
+{
+return $this->a * $this->a;
+}
+public function getPerimeter()
+{
+return 4 * $this->a;
+}
+}
+class Rectangle implements Figure
+{
+private $a;
+private $b;
+
+public function __construct($a, $b)
+{
+$this->a = $a;
+$this->b = $b;
+}
+public function getSquare()
+{
+return $this->a * $this->b;
+}
+public function getPerimeter()
+{
+return 2 * ($this->a + $this->b);
+}
 }
 
-class Employee implements iEmployee
+class Cube implements Figure3d
 {
-private $name;
-private $age;
-private $salary;
+private $a;
 
-public function setName($name)
+public function __construct($a)
 {
-$this->name = $name;
+$this->a = $a;
 }
-public function getName()
+public function getVolume()
 {
-return $this->name;
+return pow($this->a, 3);
 }
-public function setAge($age)
+public function getSurfaceSquare()
 {
-$this->age = $age;
-}
-public function getAge()
-{
-return $this->age;
-}
-public function setSalary($salary)
-{
-$this->salary = $salary;
-}
-public function getSalary()
-{
-return $this->salary;
+return 6 * $this->a * $this->a;
 }
 }
-$e = new Employee;
-$e->setName('Вася');
-$e->setSalary(1000);
-echo $e->getName().' '.$e->getSalary();// Вася 1000
+$arr[] = new Cube(3);
+$arr[] = new Quadrate(3);
+$arr[] = new Cube(4);
+$arr[] = new Rectangle(1, 2);
+$arr[] = new Quadrate(4);
+$arr[] = new Cube(5);
+$arr[] = new Rectangle(2, 3);
+$arr[] = new Quadrate(5);
+$arr[] = new Rectangle(3, 4);
+
+foreach ($arr as $obj) {
+if ($obj instanceof Figure) {
+echo $obj->getSquare().'<br>';
+}
+}
+foreach ($arr as $obj) {
+if ($obj instanceof Figure) {
+echo $obj->getSquare().'<br>';
+} elseif ($obj instanceof Figure3d) {
+echo $obj->getSurfaceSquare().'<br>';
+}
+}
 
 ?>	
