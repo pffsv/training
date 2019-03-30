7d1d52a88426690824c47430a9e7ac0d8be9633d
pffsv <pffsv@mail.ru>
2019-03-30 13:44:31 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,49 +1,32 @@
 <?php
 /*
-95.Интерфейсы и instanceof
+96.Несколько интерфейсов
 
-1.Сделайте интерфейс Figure3d (трехмерная фигура),
-который будет иметь метод getVolume (получить объем) и
-метод getSurfaceSquare (получить площадь поверхности).
-2.Сделайте класс Cube (куб), который будет
-реализовывать интерфейс Figure3d.
-3.Создайте несколько объектов класса Quadrate,
-несколько объектов класса Rectangle и несколько
-объектов класса Cube. Запишите их в массив $arr
-в случайном порядке.
-4.Переберите циклом массив $arr и выведите на
-экран только площади объектов реализующих интерфейс Figure.
-5.Переберите циклом массив $arr и выведите для плоских
-фигур их площади, а для объемных - площади их поверхности.
+1.Сделайте так, чтобы класс Rectangle также
+реализовывал два интерфейса: и Figure, и Tetragon.
+2.Сделайте интерфейс Circle (круг) с методами
+getRadius (получить радиус) и getDiameter (получить диаметр).
+3.Сделайте так, чтобы класс Disk также реализовывал
+два интерфейса: и Figure, и Circle.
 */
 interface Figure
 {
 public function getSquare();
 public function getPerimeter();
 }
-interface Figure3d
+interface Tetragon
 {
-public function getVolume();
-public function getSurfaceSquare();
+public function getA();
+public function getB();
+public function getC();
+public function getD();
 }
-class Quadrate implements Figure
+interface Circle
 {
-private $a;
-
-public function __construct($a)
-{
-$this->a = $a;
+public function getRadius();
+public function getDiameter();
 }
-public function getSquare()
-{
-return $this->a * $this->a;
-}
-public function getPerimeter()
-{
-return 4 * $this->a;
-}
-}
-class Rectangle implements Figure
+class Rectangle implements Figure, Tetragon
 {
 private $a;
 private $b;
@@ -51,7 +34,22 @@ private $b;
 public function __construct($a, $b)
 {
 $this->a = $a;
-$this->b = $b;
+}
+public function getA()
+{
+return $this->a;
+}
+public function getB()
+{
+return $this->b;
+}
+public function getC()
+{
+return $this->a;
+}
+public function getD()
+{
+return $this->b;
 }
 public function getSquare()
 {
@@ -59,47 +57,27 @@ return $this->a * $this->b;
 }
 public function getPerimeter()
 {
-return 2 * ($this->a + $this->b);
+return 2 * $this->a * $this->b;
 }
+}
+class Disk implements Figure, Circle
+{
+public function getSquare()
+{
+
+}
+public function getPerimeter()
+{
+
 }
 
-class Cube implements Figure3d
+public function getRadius()
 {
-private $a;
 
-public function __construct($a)
+}
+public function getDiameter()
 {
-$this->a = $a;
-}
-public function getVolume()
-{
-return pow($this->a, 3);
-}
-public function getSurfaceSquare()
-{
-return 6 * $this->a * $this->a;
-}
-}
-$arr[] = new Cube(3);
-$arr[] = new Quadrate(3);
-$arr[] = new Cube(4);
-$arr[] = new Rectangle(1, 2);
-$arr[] = new Quadrate(4);
-$arr[] = new Cube(5);
-$arr[] = new Rectangle(2, 3);
-$arr[] = new Quadrate(5);
-$arr[] = new Rectangle(3, 4);
 
-foreach ($arr as $obj) {
-if ($obj instanceof Figure) {
-echo $obj->getSquare().'<br>';
-}
-}
-foreach ($arr as $obj) {
-if ($obj instanceof Figure) {
-echo $obj->getSquare().'<br>';
-} elseif ($obj instanceof Figure3d) {
-echo $obj->getSurfaceSquare().'<br>';
 }
 }
 
