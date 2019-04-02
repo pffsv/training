1b80417015320713630d4cdb255f009472bb7ff9
pffsv <pffsv@mail.ru>
2019-04-03 06:02:20 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,47 +1,94 @@
 <?php
 /*
-99.Работа с трейтами
+100.Несколько трейтов
 
-1.Реализуйте класс Country (страна) со свойствами
-name (название), age (возраст), population
-(количество населения) и геттерами для них.
-Пусть наш класс для сокращения своего кода
-использует уже созданный нами трейт Helper.
+1.Сделайте 3 трейта с названиями Trait1, Trait2 и Trait3.
+Пусть в первом трейте будет метод method1, возвращающий 1,
+во втором трейте - метод method2, возвращающий 2,
+а в третьем трейте - метод method3, возвращающий 3.
+Пусть все эти методы будут приватными.
+3.Сделайте класс Test, использующий все три созданных
+нами трейта. Сделайте в этом классе публичный метод getSum,
+возвращающий сумму результатов методов подключенных трейтов.
 */
-trait Helper
+trait Trait1
 {
-private $name;
-private $age;
-
-public function getName()
+private function method1()
 {
-return $this->name;
-}
-
-public function getAge()
-{
-return $this->age;
+return 1;
 }
 }
-class Country
+trait Trait2
 {
-use Helper;
-
-private $population;
-
-function __construct($name, $age, $population)
+private function method2()
 {
-$this->name = $name;
-$this->age = $age;
-$this->population = $population;
-}
-public function getPopulation()
-{
-return $this->population;
+return 2;
 }
 }
+trait Trait3
+{
+private function method3()
+{
+return 3;
+}
+}
+class Test
+{
+use Trait1, Trait2, Trait3;
 
-$c = new Country('Хоббитания', 1200, 500);
-echo $c->getName().' '.$c->getAge().' '.$c->getPopulation();// Хоббитания 1200 500
+public function getSum()
+{
+return self::method1() + self::method2() + self::method3();
+}
+}
+echo (new Test)->getSum();// 6
 
+/*
+101.Разрешение конфликтов в трейтах
+
+1.Сделайте 3 трейта с названиями Trait1, Trait2 и Trait3.
+Пусть в первом трейте будет метод method, возвращающий 1,
+во втором трейте - одноименный метод, возвращающий 2,
+а в третьем трейте - одноименный метод, возвращающий 3.
+2.Сделайте класс Test, использующий все три созданных
+нами трейта. Сделайте в этом классе метод getSum,
+возвращающий сумму результатов методов подключенных трейтов.
+*/
+trait Trait1
+{
+private function method()
+{
+return 1;
+}
+}
+trait Trait2
+{
+private function method()
+{
+return 2;
+}
+}
+trait Trait3
+{
+private function method()
+{
+return 3;
+}
+}
+class Test
+{
+use Trait1, Trait2, Trait3 {
+Trait1::method insteadof Trait2;
+Trait2::method insteadof Trait3;
+Trait1::method as method1;
+Trait2::method as method2;
+Trait3::method as method3;
+}
+
+public function getSum()
+{
+return self::method1() + self::method2() + self::method3();
+}
+}
+echo (new Test)->getSum();// 6
 ?>	
