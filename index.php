04f906f47ae6e56861c644cd8eb19917954a6ced
pffsv <pffsv@mail.ru>
2019-04-10 19:21:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 //1.Hello World
-   
+
     echo 'Hello World!'; // Выводим строку Hello World!
     echo '<br />';
     echo '<b>Hello</b>';
-//  echo "\u{03B2}";
+    echo "\u{03B2}";
 ?>
 <p>А это уже HTML-текст!</p>
