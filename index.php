20bbaadc00aa0eb750666fd3444bf8fe3ce78054
pffsv <pffsv@mail.ru>
2019-04-12 05:47:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?
-   $var = "5";
-   $var1 = 5;
-   echo(gettype($var));
-   echo "<br>";
-   echo(gettype($var1));
-?>
+   echo(4<<2); // равно 16 
+   echo"<br>"; 
+   echo(5>>1); // равно 2
+   echo"<br>"; 
+   echo(6&5); // равно 4
+   echo"<br>"; 
+   echo(6|5); // равно 7
+   echo"<br>"; 
+   echo(6^5); // равно 3
+>?
