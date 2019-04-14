e36f6a608f4795ec217d67d9f869ca5d507fa405
pffsv <pffsv@mail.ru>
2019-04-15 05:31:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?
-   $action = $HTTP_POST_VARS["action"];
-   if ( empty($action) )
-   {
-   ?>
-      <HTML>
-      <HEAD>
-      <TITLE>Примерчик</TITLE>
-      </HEAD>
-      <BODY>
-      <center>
-      <table width=1 border=0>
-      <form action=test1.php method=post>
-         <input type=hidden name=action value=post>
-         <tr><td colspan=2>Сообщение<br><textarea cols=50 rows=8 name=msg>
-         <? echo $msg; ?>
-         </textarea></td></tr>
-         <tr><td colspan=2><input type=submit value='Добавить'></td></tr>
-      </form>
-      </table>
-      </center>
-      </BODY>
-      </HTML>
-   <?
-   }
+   $msg = substr($HTTP_POST_VARS["msg"],0,1024);
+   $msg = htmlspecialchars($msg);
+   print "<P>".$msg."</P>\n";
 ?>
