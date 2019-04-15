85818a14d9f55ff9690470ca313b915b39a0ea96
pffsv <pffsv@mail.ru>
2019-04-16 05:29:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?
-   $msg = substr($HTTP_POST_VARS["msg"],0,1024);
-   $msg = htmlspecialchars($msg);
-   print "<P>".$msg."</P>\n";
+   $str = "   Hello, world!   ";
+   $str1 = trim("   Hello, world!   ");
+   $str_len = strlen($str);
+   $str1_len = strlen($str1);
+   echo(" размер исходной строки '$str' = $str_len, <br>
+   размер строки после удаления пробелов = $str1_len");    
 ?>
