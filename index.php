24133ec5771f540bbea1756668aea17050dbeacd
pffsv <pffsv@mail.ru>
2019-04-17 05:32:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?
-   $product = "maxtor/203-5505";
-   $str = sscanf($product,"maxtor/%3d-%4d");
-   echo ("$str[0]-$str[1]");
-
-   $date = "august 10 2003";
-   list($month, $day, $year) = sscanf($date, "%s %d %d");
-   echo("Date: $day-".substr($month,0,3)."-$year");
-
+   $car[10] = "passenger car";
+   $car[20] = "land-rover";
+   $car[30] = "station-wagon";
+   $car[] = "victoria";
+   echo($car[31]);
 ?>
