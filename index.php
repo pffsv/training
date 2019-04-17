73a8594d3056909971bfd15016cbb82290d7c5f2
pffsv <pffsv@mail.ru>
2019-04-18 05:31:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,17 @@
    {
       echo("$index -> $val <br>");
    }
+
+
+   echo(
+      "available cars: <br> <ul>"
+   );
+   $car = array("passenger car", "land-rover",
+      "station-wagon","victoria");
+   foreach($car as $val)
+   {
+      echo("<li>$val</li>\n");
+   }
+   echo("</ul>");
+
 ?>
