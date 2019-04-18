41fbb32f495960bb9d6d026abea7a07565aaae04
pffsv <pffsv@mail.ru>
2019-04-19 05:29:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?
-   $array1 = $array2 = array("pict10.gif", "pict2.gif", "pict20.gif", "pict1.gif");
-   echo ("обычная сортировка:"); echo ("<br>");
-   sort($array1);
-   print_r($array1);
-   echo ("<br>"); echo ("естественная сортировка:"); echo ("<br>");
-   natsort($array2);
-   print_r($array2);
+   $car = array("passenger car", "land-rover",
+      "station-wagon","victoria");
+   foreach($car as $index => $val)
+   {
+      echo("$index -> $val <br>");
+   }
+//Как видно из синтаксиса, переменная $key необязательна и может быть опущена:
+   echo(
+      "available cars: <br> <ul>"
+   );
+   $car = array("passenger car", "land-rover",
+      "station-wagon","victoria");
+   foreach($car as $val)
+   {
+      echo("<li>$val</li>\n");
+   }
+   echo("</ul>");
 ?>
