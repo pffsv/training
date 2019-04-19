4d05fcd01bd0e5dbd22aea0ca615906acf6e2bff
pffsv <pffsv@mail.ru>
2019-04-20 06:15:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?
-   $number = array ("1"=>"15", "2"=>"20", "3"=>"25");
-   function printarray ($item, $key) 
-   {
-      echo "$key=>$item<br>\n";
-   }
-   function add_array (&$item, $key) 
-   // параметр $item передаем по ссылке, так как            
-   // его нам надо изменять
-   {
-      $item = $item + 1;
-   }
-   echo("Before:<br>");
-   array_walk ($number, 'printarray');
-   echo("After:<br>");
-   array_walk ($number, 'add_array');
-   array_walk ($number, 'printarray');
+  function get_sum()
+  {
+    $var = 5;
+    $var1 = 10;
+    $sum = $var + $var1;
+    return $sum;
+  }
+  echo(get_sum()); // выводит 15
 ?>
