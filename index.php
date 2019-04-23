383b560f470fc27fa3b096ef8af3955901ae8967
pffsv <pffsv@mail.ru>
2019-04-24 05:24:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?
-  $dir = opendir (".");
-  while ( $file = readdir ($dir))
+  function full_del_dir ($directory)
   {
-    if (( $file != ".") && ($file != ".."))
+  $dir = opendir($directory);
+  while(($file = readdir($dir)))
+  {
+    if ( is_file ($directory."/".$file))
     {
-      echo "$file<br>";
+      unlink ($directory."/".$file);
+    }
+    else if ( is_dir ($directory."/".$file) &&
+             ($file != ".") && ($file != ".."))
+    {
+      full_del_dir ($directory."/".$file);  
     }
   }
   closedir ($dir);
+  rmdir ($directory);
+  echo("Каталог успешно удален");
+  }
+  full_del_dir ("c:/temp")
 ?>
