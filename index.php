14c9f215777abd79818607d6cfe6b74c6730f233
pffsv <pffsv@mail.ru>
2019-04-25 05:30:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?
-  // получаем www-адрес (имя хоста) из url
-  $url = "http://www.php.net/download.html";
-  preg_match("/^(http:\/\/)?([^\/]+)/i", $url, $matches);
-  $host = $matches[2];
-  echo("www-адрес: $host");
-  echo("<br>");
-  // получаем последние два сегмента имени хоста (доменное имя)
-  preg_match("/[^\.\/]+\.[^\.\/]+$/", $host, $matches);
-  echo "доменное имя: {$matches[0]}\n";
+  $str = "May 15, 2003";
+  $pattern = "/(\w+) (\d+), (\d+)/i";
+  $replacement = "1 \${1} \$3";
+  print preg_replace($pattern, $replacement, $str);
+
+//Результат:
+//1 May 2003
+
 ?>
