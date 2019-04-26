1e6d5efc09e36b83898e1b4aa9fdcb0530037f88
pffsv <pffsv@mail.ru>
2019-04-27 05:51:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
-
 <?
-//Регистрация на FTP-сервере
-  $user = "maks";
-  $password = "password";
+  $host = "ftp.server.ru";
+  $port = 21;
+  $user = "anonymous";
+  $passwrod = "password";
+  $connect = ftp_connect($host, $port, 150);
+  if(!$connect)
+  {
+    exit();  
+  }
   $result = ftp_login($connect, $user, $password);
+  if($result)
+  {
+    // сохраняем имя текущего рабочего каталога
+    $current_dir = ftp_pwd($connect);
+    // переходим в родительский каталог 
+    ftp_cdup($connect);
+    // сохраняем имя нового каталога
+    $new_dir = ftp_pwd($connect);
+  }
+  else
+  {
+    ftp_quit($connect);
+    exit();
+  }
+  // Закрываем соединение
+  ftp_quit($connect);
 ?>
