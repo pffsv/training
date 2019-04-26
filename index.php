0ae839c6b263fa004632f71d558406b16ff12c01
pffsv <pffsv@mail.ru>
2019-04-27 05:50:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,15 @@
+
 <?
-  function my_htmlspecialchats($document)
+//Соединение с удаленным FTP-сервером
+  $host = "ftp://ftp.server.ru";
+  $connect = ftp_connect($host);
+  if(!$connect)
   {
-    $search = array ("']*?>.*?'si",  
-                     "'<[\/\!]*?[^<>]*?>'si",           
-                     "'([\r\n])[\s]+'",                 
-                     "'&(quot|#34);'i",                 
-                     "'&(amp|#38);'i",
-                     "'&(lt|#60);'i",
-                     "'&(gt|#62);'i",
-                     "'&(nbsp|#160);'i",
-                     "'&(iexcl|#161);'i",
-                     "'&(cent|#162);'i",
-                     "'&(pound|#163);'i",
-                     "'&(copy|#169);'i",
-                     "'&#(\d+);'e");
-    $replace = array ("",
-                      "",
-                      "\\1",
-                      "\"",
-                      "&",
-                      "<",
-                      ">",
-                      " ",
-                      chr(161),
-                      chr(162),
-                      chr(163),
-                      chr(169),
-                      "chr(\\1)");
-    $text = preg_replace ($search, $replace, $document);
-    return $text;
+    echo("Ошибка соединения");
+    exit;
   }
-  $doc = "alert;"; // напишем java-скрипт
-  my_htmlspecialchats($doc);  // выводит "alert;"
-  echo($doc);
+  else
+  {
+    echo("Соединение установлено");  
+  }
 ?>
