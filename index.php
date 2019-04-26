00044d58da83dc7a729487a91887470ddd984682
pffsv <pffsv@mail.ru>
2019-04-27 05:53:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?
-  $file = "/web/user/file.txt";
-  $file_size = ftp_size($connect, $file);
-  if ($file_size == -1)
-  {
-    echo("Размер файла не определен");
-  }
-  else
-  {
-    echo("Файл $file имеет размер $file_size байт");
-  }
+  $pasv_flag = true;
+  $pasv_result = $pasv_flag ? "enabled" : "disabled";
+  ftp_pasv($connect, $pasv_flag);
 ?>
