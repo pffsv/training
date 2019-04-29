0d7e4eabbb9a60976dae114c93b61af68bc1b255
pffsv <pffsv@mail.ru>
2019-04-30 05:30:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?
+  $error = "";
   $action = $HTTP_POST_VARS["action"];
   if (!empty($action)) 
   {
-    if (empty($name)) 
-    {
-     // код, для случая, когда не введено имя
-    }
-    if (!empty($email))
-    {
-       // код, для случая, когда не введен e-mail
-    }
-  // дальнейший код скрипта
-  }
-  if (empty($action)) 
-  {
-?>
-  <!-- здесь пишем HTML-код формы, в которой вводится информация -->
-<?
-  }
-?>
+    $name = trim($name);
+    $msg = trim($msg);
+      if (empty($msg)) // если не введено сообщение
+      {
+        $action = "";
+        $error = $error."<LI>Вы не ввели сообщение\n";
+      }
+      if (empty($name)) // если не введено имя
+      {
+        $action = "";
+        $error = $error."<LI>Вы не ввели имя\n";
+      }
+      if (!empty($email))
+      /* если введен e-mail, то проверяем с помощью регулярного выражения
+      правильность ввода */
+      {
+        if (!preg_match("/[0-9a-z_]+@[0-9a-z_^\.]+\.[a-z]{2,3}/i", $email))
+        {
+          $action = "";
+          $error = $error."<LI> Неверно введен е-mail.&nbsр Введите e-mail
+          в виде <i>softtime@softtime.ru</i> \n";
+        }
+      }
