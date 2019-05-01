22145678b6b64d689328a2f1e79da70fff66966b
pffsv <pffsv@mail.ru>
2019-05-02 05:51:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-   define('CONSTANT1', 15); 
-   define('CONSTANT2', "\x20"); //Шестнадцатеричный код символа пробела
-   define('CONSTANT3', 'Hello'); 
-   echo CONSTANT1; 
-   echo CONSTANT2; 
-   echo CONSTANT3; 
+   echo (4<<2); //16
+   echo "<br />"; 
+   echo (5>>1); //2
+   echo "<br />"; 
+   echo (6&5); //4
+   echo "<br />"; 
+   echo (6|5); //7
+   echo "<br />"; 
+   echo (6^5); //3
 ?>
