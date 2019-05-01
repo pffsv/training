1e41aa7cc31862e9f4b29841c71201dfb87a1ed4
pffsv <pffsv@mail.ru>
2019-05-02 05:52:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-   echo (4<<2); //16
-   echo "<br />"; 
-   echo (5>>1); //2
-   echo "<br />"; 
-   echo (6&5); //4
-   echo "<br />"; 
-   echo (6|5); //7
-   echo "<br />"; 
-   echo (6^5); //3
+   $a = 1; 
+   $b = null; 
+   $c = 2 * 3; 
+   $d = true; 
+   if($a) echo "if(\$a);<br />"; 
+   if($b) echo "if(\$b);<br />"; 
+   if($c) echo "if(\$c);<br />"; 
+   if(!$d) echo "if(!\$d);<br />"; 
+   if($a < $c) echo "\$a < \$c;<br />"; 
 ?>
