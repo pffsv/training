10f8871e3e72489bd55c8833cb741cc07bef877f
pffsv <pffsv@mail.ru>
2019-05-04 04:52:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-$myString = "Hello!";
-echo $myString;
-echo "<h5>I love using PHP!</h5>";
+// This won't work because of the quotes around specialH5!
+echo "<h5 class="specialH5">I love using PHP!</h5>";  
+
+// OK because we escaped the quotes!
+echo "<h5 class=\"specialH5\">I love using PHP!</h5>";  
+
+// OK because we used an apostrophe '
+echo "<h5 class='specialH5'>I love using PHP!</h5>";  
 ?>
