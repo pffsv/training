c22f1c17c9a0cf6b8cd7e73cc0fbf6e7a8c3e846
pffsv <pffsv@mail.ru>
2019-05-04 04:54:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 $my_string = "Hello Bob.  My name is: ";
-$my_number = 4;
-$my_letter = a;
-echo $my_string;
-echo $my_number;
-echo $my_letter;
+echo "$my_string Bobettta <br />";
+echo "Hi, I'm Bob.  Who are you? $my_string <br />";
+echo "Hi, I'm Bob.  Who are you? $my_string Bobetta";
 ?>
