0807cc33f38a2f4def9d33705fab2c95a0548150
pffsv <pffsv@mail.ru>
2019-05-06 05:09:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
-$addition = 2 + 4; 
-$subtraction = 6 - 2; 
-$multiplication = 5 * 3; 
-$division = 15 / 3; 
-$modulus = 5 % 2; 
-echo "Пример сложения: 2 + 4 = ".$addition."<br />"; 
-echo "Пример вычитания: 6 - 2 = ".$subtraction."<br />"; 
-echo "Пример умножения:  5 * 3 = ".$multiplication."<br />"; 
-echo "Пример деления нацело: 15 / 3 = ".$division."<br />"; 
-echo "Пример остатка от деления: 5 % 2 = " . $modulus;
+$a_string = "Hello";
+$another_string = " Billy";
+$new_string = $a_string . $another_string;
+echo $new_string . "!";
 ?>
