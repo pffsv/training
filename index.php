cde96f3ce972f6df215bf151ced99834b244371c
pffsv <pffsv@mail.ru>
2019-05-07 05:14:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-echo "Hello World!"; // This will print out Hello World!
-echo "<br />Psst...You can't see my PHP comments!"; // echo "nothing";
-// echo "My name is Humperdinkle!";
-# echo "I don't do anything either";
-/* This Echo statement will print out my message to the
-the place in which I reside on.  In other words, the World. */
-echo "Hello World!"; 
-/* echo "My name is Humperdinkle!";
-echo "No way! My name is Uber PHP Programmer!";
-*/
+$my_name = "Вася";
+
+if ( $my_name == "Вася" ) {
+  echo "Привет, Вася!<br />";
+}
+echo "Добро пожаловать на мою страницу!";
 ?>
