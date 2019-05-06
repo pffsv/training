2842a82cab6b2fced17a269b5d5a9eca6d55f14a
pffsv <pffsv@mail.ru>
2019-05-07 05:12:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-$a_string = "Hello";
-$another_string = " Billy";
-$new_string = $a_string . $another_string;
-echo $new_string . "!";
+echo "Hello World!"; // This will print out Hello World!
+echo "<br />Psst...You can't see my PHP comments!"; // echo "nothing";
+// echo "My name is Humperdinkle!";
+# echo "I don't do anything either";
+/* This Echo statement will print out my message to the
+the place in which I reside on.  In other words, the World. */
+echo "Hello World!"; 
+/* echo "My name is Humperdinkle!";
+echo "No way! My name is Uber PHP Programmer!";
+*/
 ?>
