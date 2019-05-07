a1358e3e4383661e1eb578ef382201ad94189fd3
pffsv <pffsv@mail.ru>
2019-05-08 05:29:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
 <?php
-$destination = "New York";
-echo "Отправляемся в $destination<br />";
-switch ($destination){
-  case "Las Vegas":
-    echo "Берем с собой $5000! Вегас детка!";
-    break;
-  case "Amsterdam":
-    echo "Парочка газет, фольга...";
-    break;  
-  case "Egypt":
-    echo "50 бутылок солнцезащитного крема.";
-    break;  
-  case "Tokyo":
-    echo "Берем деньги на сувениры.";
-    break;
-  case "Caribbean Islands":
-    echo "Захватите трусы для плавания!";
-    break;  
-  default:
-    echo "Бельишко чистое не забудьте!";
-    break;
+function myCompanyName(){
+    echo "Вы находитесь на блоге myblaze.ru <br />";
+}
+echo "Привет всем! <br />";
+myCompanyName();
+echo "Рад вас видеть! <br />";
 ?>
