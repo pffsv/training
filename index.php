fee9bd892c7c03c75ab24d3c93b61c14ce38fe7f
pffsv <pffsv@mail.ru>
2019-05-08 05:30:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-function mySum($numX, $numY){
-    $total = $numX + $numY;
-    return $total; 
-}
-$myNumber = 0;
-echo "Перед использованием функции, myNumber = ". $myNumber ."<br />";
-$myNumber = mySum(3, 4); // Сохраняем результат вызова функции mySum в переменную $myNumber
-echo "После вызова функции, myNumber = " . $myNumber ."<br />";
+$employee_array[0] = "Петя";
+$employee_array[1] = "Вася";
+$employee_array[2] = "Ваня";
+$employee_array[3] = "Федор Петрович";
+echo "Два моих работника:  "
+. $employee_array[0] . " и " . $employee_array[1]; 
+echo "<br />И еще два работника: " 
+. $employee_array[2] . " и " . $employee_array[3];
 ?>
