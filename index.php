64db39219cdf4d2635e1b6319130bc1d994d4fa2
pffsv <pffsv@mail.ru>
2019-05-08 05:30:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,14 @@ echo "Два моих работника:  "
 . $employee_array[0] . " и " . $employee_array[1]; 
 echo "<br />И еще два работника: " 
 . $employee_array[2] . " и " . $employee_array[3];
+
+$salaries["Петя"] = 2000;
+$salaries["Вася"] = 4000;
+$salaries["Ваня"] = 600;
+$salaries["Даша"] = 0;
+
+echo "Петя получает - $" . $salaries["Петя"] . "<br />";
+echo "Вася получает - $" . $salaries["Вася"] . "<br />";
+echo "А Ваня получает всего - $" . $salaries["Ваня"] . "<br />";
+echo "Даша в этом месяце не работала, поэтому - $" . $salaries["Даша"]
 ?>
