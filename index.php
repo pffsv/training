7c066a827af8aef42c82ea7a7c7e573bcee0103e
pffsv <pffsv@mail.ru>
2019-05-09 05:54:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
 
-//Пользователь отправляет нам данные и надеется перенаправить нам на свой сайт
-$userInput = "Смотрите, сейчас я перенаправлю вас на example.com! Хахаха!
-  <script type='text/javascript'>
-  window.location = 'https://www.example.com/'
-  </script>'";
-  
-//Но мы это предусмотрели и использовали htmlentities
-$userInputEntities = htmlentities($userInput);
-
-//И теперь без опасений используем присланные данные
-echo $userInputEntities;
+$myFile = "testFile.txt";
+$fh = fopen($myFile, 'r');
+$theData = fread($fh, filesize($myFile));
+fclose($fh);
+echo $theData;
 
 ?>
