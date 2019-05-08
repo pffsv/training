999c736e7f846371adb2a959e44753a8e87aaf73
pffsv <pffsv@mail.ru>
2019-05-09 05:53:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 
- if (!empty($_POST["user_name"])&&!empty($_POST["age"]))
- {
- echo "Получены новые вводные:<br>";
- echo "имя - ";
- echo $_POST["user_name"];
- echo "<br>возраст - ";
- echo $_POST["age"];
- echo " лет";
- }
- else
- {
- echo "Переменные не дошли. Проверьте все еще раз.";
- }
+//Пользователь отправляет нам данные и надеется перенаправить нам на свой сайт
+$userInput = "Смотрите, сейчас я перенаправлю вас на example.com! Хахаха!
+  <script type='text/javascript'>
+  window.location = 'https://www.example.com/'
+  </script>'";
+  
+//Но мы это предусмотрели и использовали htmlentities
+$userInputEntities = htmlentities($userInput);
+
+//И теперь без опасений используем присланные данные
+echo $userInputEntities;
 
 ?>
