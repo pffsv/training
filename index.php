8006d3aa960e56a6ef5ea835d109f6751361f12e
pffsv <pffsv@mail.ru>
2019-05-09 05:52:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<html>
-<head>
-<title>Страница с примером передачи переменных с помощью Post</title>
-</head>
-<body>
+<?php
 
-<form method="post" action="index.php">Заполняем поля для передачи информации:<br><br>
- Укажите Ваше имя: <input name="user_name" type="text" maxlength="20" size="25" value="" />
-<br><br> Укажите Ваш возраст: <input name="age" type="text" maxlength="2" size="3" value="" />
-<br><br> <input type=submit value="Передать информацию"></form>
-</body>
-</html>
+ if (!empty($_POST["user_name"])&&!empty($_POST["age"]))
+ {
+ echo "Получены новые вводные:<br>";
+ echo "имя - ";
+ echo $_POST["user_name"];
+ echo "<br>возраст - ";
+ echo $_POST["age"];
+ echo " лет";
+ }
+ else
+ {
+ echo "Переменные не дошли. Проверьте все еще раз.";
+ }
+
+?>
