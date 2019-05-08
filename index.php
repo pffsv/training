142084b951735e492d73169fb31de9af430a1e14
pffsv <pffsv@mail.ru>
2019-05-09 05:51:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
-$mnojitel = 5; 
-echo "<table border=\"1\" align=\"center\">";
-echo "<tr><th>Переменная counter</th>";
-echo "<th>counter * mnojitel</th></tr>";
-for ( $counter = 3; $counter <= 11; $counter +=3) {
-  echo "<tr><td>";
-  echo $counter;
-  echo "</td><td>";
-  echo $mnojitel * $counter;
-  echo "</td></tr>";
+$employeeAges;
+$employeeAges["Lisa"] = "28";
+$employeeAges["Jack"] = "16";
+$employeeAges["Ryan"] = "35";
+$employeeAges["Rachel"] = "46";
+$employeeAges["Grace"] = "34";
+
+foreach( $employeeAges as $key => $value){
+  echo "Name: $key, Age: $value <br />";
 }
-echo "</table>";
 ?>
