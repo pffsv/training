c71bc2f4da13cdb2ee0396dc969a0d5b2a3d2e8a
pffsv <pffsv@mail.ru>
2019-05-10 05:01:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
 $myFile = "testFile.txt";
-$fh = fopen($myFile, 'r');
-$theData = fread($fh, filesize($myFile));
+$fh = fopen($myFile, 'w') or die("can't open file");
+$stringData = "Первая строчка\n";
+fwrite($fh, $stringData);
+$stringData = "Вторая строчка\n";
+fwrite($fh, $stringData);
 fclose($fh);
-echo $theData;
-
 ?>
