cd48d1f310acbd27565f4d041d13804de14b12a7
pffsv <pffsv@mail.ru>
2019-05-10 05:03:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,13 @@
 <?php
 //Строка - шаблон
-function get_web_page( $url )
-{
-  $uagent = "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.14";
-
-  $ch = curl_init( $url );
-
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   // возвращает веб-страницу
-  curl_setopt($ch, CURLOPT_HEADER, 0);           // не возвращает заголовки
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);   // переходит по редиректам
-  curl_setopt($ch, CURLOPT_ENCODING, "");        // обрабатывает все кодировки
-  curl_setopt($ch, CURLOPT_USERAGENT, $uagent);  // useragent
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120); // таймаут соединения
-  curl_setopt($ch, CURLOPT_TIMEOUT, 120);        // таймаут ответа
-  curl_setopt($ch, CURLOPT_MAXREDIRS, 10);       // останавливаться после 10-ого редиректа
-
-  $content = curl_exec( $ch );
-  $err     = curl_errno( $ch );
-  $errmsg  = curl_error( $ch );
-  $header  = curl_getinfo( $ch );
-  curl_close( $ch );
-
-  $header['errno']   = $err;
-  $header['errmsg']  = $errmsg;
-  $header['content'] = $content;
-  return $header;
-}
+$result = get_web_page( "https://ya.ru" );
+if (($result['errno'] != 0 )||($result['http_code'] != 200))
+    {
+  echo $result['errmsg'];
+  }
+else
+  {
+  $page = $result['content'];
+  echo $page;
+  }
 ?>
