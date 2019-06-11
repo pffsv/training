27b3aa035633b0c0d124e9df602830d8ad2bdc5f
pffsv <pffsv@mail.ru>
2019-06-12 05:18:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Документ без названия</title>
-</head>
-<body>
-<h1>Привет мир!</h1>
-<hr width="50%" color="#037FFC" >
-<p>Здравствуйте уважаемые будущие веб мастера.
-После долгих колебаний я всё-таки решил тоже
-покопать на ниве обучения сайтостроению, хотя более перекопанной
-нивы наверное трудно себе представить, так как о том,
-как создать сайт на WordPress, в интернете описано уже несколько миллионов раз.
-И всё-ж, и всё-ж.</p>
-<hr width="50%" color="#037FFC" >
-</body>
-</html>
+#header{
+height: 80px;
+background-color: #25B33f;
+margin-bottom: 10px;
+}
+#sidebar{
+background-color: #2FF553;
+margin-bottom: 10px;
+width: 180px;
+padding: 10px;
+float: right;
+}
