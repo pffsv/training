891003fc3efe3b11e289e33a353dd1b33f505338
pffsv <pffsv@mail.ru>
2019-06-12 05:18:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
-#header{
-height: 80px;
-background-color: #25B33f;
-margin-bottom: 10px;
+<?php
+$limit_login_my_error_shown = false; /* have we shown our stuff? */
+$limit_login_just_lockedout = false; /* started this pageload??? */
+$limit_login_nonempty_credentials = false; /* user and pwd nonempty */
+global $limit_login_my_error_shown;
+$limit_login_my_error_shown = true;
+if (!empty($_COOKIE[LOGGED_IN_COOKIE])) {
+$_COOKIE[LOGGED_IN_COOKIE] = '';
 }
-#sidebar{
-background-color: #2FF553;
-margin-bottom: 10px;
-width: 180px;
-padding: 10px;
-float: right;
-}
+?>
