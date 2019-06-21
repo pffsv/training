c4ca9e2477f521f22343def66df4029273d855b7
pffsv <pffsv@mail.ru>
2019-06-22 05:54:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <?php
-class parent_class{                 //Объявили родительский класс 
-  public $my_var='value ';          //Объявили свойство класса
-     
-  final function my_func(){         //Определяем и финализируем метод 
-    echo $this->my_var;             //Выводим значение свойства
-  } 
-}
-  
-class descendant_class extends parent_class{//Создаем класс-потомок
-     
-  function my_func(){                //Переопределение вызовет ошибку!!! 
-    parent::my_func();        
-    echo 'Нельзя переопределять финализированный метод';  
+class fruit_color{
+  public $color,  $fruit;       //Объявляем свойства
+   
+  function __construct($c){     //Объявляем конструктор с одним аргументом
+    $this->color=$c;            //Задаем цвет фруктов
   }
-} 
+   
+  function set_fruit($fruit){   //Объявляем обычный метод с одним аргументом
+    $this->fruit=$fruit;        //Задаем вид фруктов
+    echo $this->color.' '.$this->fruit.'<br>';//Выводим цвет и вид 
+  }
+   
+  function __destruct(){        //Вызываем деструктор для удаления
+                                //объекта при освобождении ссылок на него
+   }
+}
+ 
+$apples=new fruit_color('Yellow'); //Создаем объект и сразу же инициализируем 
+$apples->set_fruit('orange');      //Вызываем метод объекта
+ 
+class fruit_color_2 extends fruit_color{     //Объявляем класс-потомок
+ 
+  function __construct($c_1, $c_2){          //Объявляем свой конструктор 
+    $this->color=$c_1.' and '.$c_2;          //Задаем два цвета фруктов
+  }
+}
+ 
+$apples_2=new fruit_color_2('Red', 'yellow');//Создаем объект класса-потомка
+$apples_2->set_fruit('apples');              //Вызываем метод объекта
 ?>
