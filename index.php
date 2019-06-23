87c5d601160b295d236a2f86469b1a99aba3f6cd
pffsv <pffsv@mail.ru>
2019-06-24 05:13:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <?php
-class fruit_color{
-  public $color,  $fruit;       //Объявляем свойства
-   
-  function __construct($c){     //Объявляем конструктор с одним аргументом
-    $this->color=$c;            //Задаем цвет фруктов
-  }
-   
-  function set_fruit($fruit){   //Объявляем обычный метод с одним аргументом
-    $this->fruit=$fruit;        //Задаем вид фруктов
-    echo $this->color.' '.$this->fruit.'<br>';//Выводим цвет и вид 
-  }
-   
-  function __destruct(){        //Вызываем деструктор для удаления
-                                //объекта при освобождении ссылок на него
-   }
-}
- 
-$apples=new fruit_color('Yellow'); //Создаем объект и сразу же инициализируем 
-$apples->set_fruit('orange');      //Вызываем метод объекта
- 
-class fruit_color_2 extends fruit_color{     //Объявляем класс-потомок
- 
-  function __construct($c_1, $c_2){          //Объявляем свой конструктор 
-    $this->color=$c_1.' and '.$c_2;          //Задаем два цвета фруктов
+class parent_class{
+   protected $protected_var='protected_var '; //Доступно изнутри самого класса  
+                                              //и всех классов-потомков
+   private $private_var='private_var ';       //Доступно только изнутри класса
+    
+  function all_func(){
+    echo $this->protected_var.'<br>';//Оба свойства доступны, т.к. запрашиваются 
+    echo $this->private_var.'<br>';  //изнутри класса, где были определены
   }
 }
  
-$apples_2=new fruit_color_2('Red', 'yellow');//Создаем объект класса-потомка
-$apples_2->set_fruit('apples');              //Вызываем метод объекта
+class descendant_class extends parent_class{
+  protected $protected_var='p_var ';//Переопределяем свойство, область видимости
+                                    //при этом можно было изменить на public
+  function all_func(){              //Переопределяем метод 
+    echo $this->protected_var.'<br>';//Это свойство доступно, т.к. оно protected   
+                                     //и запрашивается изнутри класса-потомка
+    echo $this->private_var.'<br>';  //Ошибка, свойство доступно только изнутри 
+                                     //класса parent_class, где было объявлено
+  }
+}
+ 
+$obj=new parent_class();      //Создаем экземпляр родительского класса 
+$obj_2=new descendant_class();//Создаем экземпляр класса-потомка
+ 
+$obj->all_func();     //Сработает без ошибок 
+//$obj_2->all_func(); //Выдаст ошибку, т.к. свойство $this->private_var
+                      //неопределено в классе-потомке и доступно только изнутри 
+                      //класса parent_class, где оно было объявлено
+ 
+//echo $obj->private_var;   //Выдаст ошибку, т.к. свойство вне класса недоступно
+//echo $obj->protected_var; //Выдаст ошибку, т.к. свойство вне классов недоступно 
+//echo $obj_2->private_var; //Выдаст ошибку, т.к. свойство неопределено в descendant_class
 ?>
