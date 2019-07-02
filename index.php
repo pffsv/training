2c0d569bfa98506b511da3afea2496363fcf6731
pffsv <pffsv@mail.ru>
2019-07-03 05:16:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,51 @@
 <?php
  
-//Создаем трейт
-trait tr_5_minus{                  
-  //Определяем один метод
-  public function _5_minus($n){    
-    echo $this->m-$n.'<br>'; 
+//Создаем внешний класс
+class outer_class{             
+  //Объявили свойство внешнего класса
+  protected $n_out=2;          
+     
+  //Определяем первый метод outer_class
+  public function func_ins_1(){
+     
+    //Здесь $this представляет внешний класс
+    echo (new class($this->n_out) extends outer_class{
+          //Объявили свойство анонимного класса
+          public $n_ins;       
+          //Конструктор
+          function __construct($arg){
+            //А здесь $this уже относится к анонимному классу
+            $this->n_ins=$arg*$arg;
+          }
+        //И сразу же обращаемся к свойству созданного 
+        //объекта анонимного класса
+        })->n_ins;.'<br>'      
+  }                                   
+     
+  //Определяем второй метод outer_class 
+  public function func_ins_2(){
+     
+    //Здесь $this представляет внешний класс
+    return new class($this->n_out) extends outer_class{
+          //Объявили константу анонимного класса
+          const a=5;           
+          //Объявили свойство анонимного класса
+          public $n_ins;       
+          //Конструктор
+          function __construct($arg){
+            //А здесь $this уже относится к анонимному классу
+            $this->n_ins=$arg*$arg;
+          }
+         }; //Возвращаем готовый анонимный объект
   }
 }
  
-//Создаем интерфейс
-interface int_5_mult{              
-  //Задаем шаблон метода
-  public function _5_plus($n);     
-}
- 
-//Создаем объект анонимного класса
-$obj_5=new class(5) implements int_5_mult{ 
-  //Подключаем интерфейс
-  use tr_5_minus;                  
-  //Объявляем свойство класса
-  public $m;                       
-   
-  //Объявляем конструктор класса
-  function __construct($arg){      
-    //Устанавливает первый член операций
-    $this->m=$arg;                  
-  }
-   
-  //Реализуем интерфейс
-  public function _5_plus($n){     
-    echo $this->m+$n.'<br>'; 
-  }
-   
-  //Создаем собственный метод
-  public function _5_mult($n){     
-    echo $this->m*$n.'<br>'; 
-  }
-}; //Не забываем про точку с запятой!!!
- 
-//Выведет '-1'
-$obj_5->_5_minus(6);               
-//Выведет '11'
-$obj_5->_5_plus(6);                
-//Выведет '30'
-$obj_5->_5_mult(6);                
+$obj=new outer_class();
+//Выведет '4'
+$obj->func_ins_1();                    
+//Выведет '4'
+echo $obj->func_ins_2()->n_ins.'<br>'; 
+//Выведет '5'
+echo $obj->func_ins_2()::a;            
  
 ?> 
