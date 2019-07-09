4121f1b74c8f9bcb329fb415b4e1416f98bcad94
pffsv <pffsv@mail.ru>
2019-07-10 05:26:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,82 @@
 <?php
  
-namespace{
-  //Объявляем константу в глобальном пространстве
-  const my_const = 'global constant';
-     
-  //Объявляем функцию в глобальном пространстве
-  function global_function(){
-    echo 'global function'.'<br>';
+//Объявили пространство имен
+namespace name_space_1{    
+  //Объявляем константу в текущем пространстве имен 
+  const current_const_1 = 'current_constant_1'; 
+ 
+  //Объявляем функцию в текущем пространстве имен   
+  function current_function_1(){
+    echo 'current_function_1'.'<br>';
   }
    
-  //Объявляем класс в глобальном пространстве
-  class global_class{
-    static function global_method(){
-      echo 'global method'.'<br>';
+  //Объявляем класс в текущем пространстве имен
+  class current_class_1{
+    static function current_method_1(){
+      echo 'current_method_1'.'<br>';
     }
   }
-   
-  //PHP обратится к \my_const 
-  //Выведет 'global constant'
-  echo namespace\my_const.'<br>';                 
-   
-  //Вызываем функцию \global_function()
-  //Выведет 'global function'
-  namespace\global_function();                    
-  //Выведет 'global method'
-  namespace\global_class::global_method();        
 }
  
  
 //Объявили пространство имен
-namespace current_name_space{                     
+namespace name_space_2{   
+  //Импортируем имя константы из name_space_1 и создаем ей псевдоним
+  use const name_space_1\current_const_1 as B;
+  //Импортируем имя функции из name_space_1 и создаем ей псевдоним
+  use function name_space_1\current_function_1 as f;
+  //Импортируем имя класса из name_space_1 и создаем ей псевдоним
+  //При этом писать слово class не нужно
+  use name_space_1\current_class_1 as CL;
+  //Импортируем имя функции из глобального пространства и создаем ей псевдоним
+  use function \global_function as gl_f;
+   
+  //Выведет 'current_constant_1'
+  echo B.'<br>'; 
+  //Выведет 'current_function_1'
+  f();           
+   
+  //Создаем объект класса name_space_1\current_class_1
+  $a=new CL;     
+  //Выведет 'current_method_1'
+  $a->current_method_1();
+  //Выведет 'global_function'
+  gl_f();         
+   
   //Объявляем константу в текущем пространстве имен 
-  const my_const = 'current_constant'; 
-    
+  const current_const_2 = 'current_constant_2'; 
+   
   //Объявляем функцию в текущем пространстве имен   
-  function current_function(){
-    echo 'current function'.'<br>';
+  function current_function_2(){
+    echo 'current_function_2'.'<br>';
   }
    
   //Объявляем класс в текущем пространстве имен
-  class current_class{
-    static function current_method(){
-      echo 'current method'.'<br>';
+  class current_class_2{
+    static function current_method_2(){
+      echo 'current_method_2'.'<br>';
     }
-  } 
+  }
+}
+ 
+ 
+namespace{
+  //Создаем псевдоним пространства имен для сокращения записи
+  use name_space_2 as ns_2;
      
-  //PHP обратится к current_name_space\my_const 
-  //Выведет 'current_constant'
-  echo namespace\my_const.'<br>';                   
+  //Объявляем функцию в глобальном пространстве
+  function global_function(){
+    echo 'global_function'.'<br>';
+  }
    
-  //Вызываем функцию current_name_space\current_function()
-  //Выведет 'current function'  
-  namespace\current_function();                     
-   
-  //Вызываем метод current_name_space\current_method()
-  //Выведет 'current method'
-  namespace\current_class::current_method();          
+  //Выведет 'current_constant_2'
+  echo ns_2\current_const_2.'<br>'; 
+  //Выведет 'current_function_2'
+  ns_2\current_function_2();        
+  //Объект класса name_space_2\current_class_2
+  $b=new ns_2\current_class_2;      
+  //Выведет 'current_method_2'
+  $b->current_method_2();           
 }
  
 ?>
-
