e726fe3ae72d5d244784f12b8535832a94a45636
pffsv <pffsv@mail.ru>
2019-07-22 05:32:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ $username = "administrator";
 $password = "12345";
 //Указываем базу данных, к которой будем  подключаться
 $db_name = "belarusweb_users";
- 
 //Создаем объект соединения с MySQL 
 $conn = new mysqli ($servername, $username, $password, $db_name);
  
@@ -23,35 +22,53 @@ if ($conn->connect_error){
   //Функция die() выводит сообщение и прекращает выполнение текущего скрипта  
   die("Соединение установлено не было.");
 }
+//Установим кодировку данных для данного соединения с MySQL, 
+//чтобы русские символы правильно отображались в базе.
+$conn->set_charset("utf8"); 
  
-//Установим кодировку данных для данного соединения с MySQL, чтобы не было 
-//проблем с отображением русских символов.
-$conn->set_charset("utf8");
- 
-//Cоздаем таблицу our_users (имя в кавычки брать не нужно)
-//В скобках перечисляем имена столбцов, а также через пробел их характеристики
-$sql = "create table our_users(".
-      "ID INT(5) UNSIGNED PRIMARY KEY AUTO_INCREMENT,".
-      "first_name VARCHAR(25) NOT NULL,".
-      "last_name VARCHAR(25) NOT NULL,".
-      "age INT(2) NOT NULL,".
-      "sex VARCHAR(8) NOT NULL,".
-      "reg_mail VARCHAR(40) NOT NULL,".
-      "reg_date TIMESTAMP NOT NULL".
-      ") CHARACTER SET utf8";  
- 
-//Выполняем запрос и если он прошел успешно, сообщаем об успехе
-if ($conn->query($sql) === true){
-  echo "Таблица успешно создана.<br>";     
-} else {
-  //Прекращаем выполнение скрипта и выводим строку с описанием ошибки
-  echo "Ошибка создания таблицы: ".$conn->error.".";   
-  //Т.к. соединение нам пока не нужно, закрываем его
-  $conn->close();
-  //Функция die() прекращает дальнейшее выполнение текущего скрипта 
-  die();
+//Чтобы не использовать повторно большие куски кода, оформим класс 
+class sql_msg{
+  //Статический метод можно будет вызывать без создания объекта
+  public static function result($sql){
+    //Будем использовать глобальную переменную
+    global $conn;
+    //Выполняем запрос и если он прошел успешно, сообщаем об успехе
+    if($conn->query($sql) === true){
+      echo "Операция успешно выполнена.<br>";     
+    }else{
+      //Прекращаем выполнение скрипта и выводим строку с описанием ошибки
+      echo "Ошибка операции: ".$conn->error.".";   
+    }
+  }
 }
+ 
+//В первых скобках перечисляем имена столбцов, 
+//во вторых - соответствующие значения
+//Порядок столбцов может быть любым, но тогда нужно поменять и 
+//порядок соответствующих им значений. Количество значений должно 
+//быть равным количеству столбцов
+$sql_1 = "INSERT INTO our_users(first_name, last_name, age, sex, reg_mail)
+      values('Сергей', 'Петров', 30, 'мужской', 'my_mail_1@tut.by')";   
+//Выполняем запрос и если он прошел успешно, сообщаем об успехе
+sql_msg::result($sql_1);
+ 
+$sql_2 = "INSERT INTO our_users(first_name, last_name, age, sex, reg_mail)
+      values('Сергей', 'Иванов', 33, 'мужской', 'my_mail_3@tut.by')"; 
+sql_msg::result($sql_2);
+ 
+$sql_3 = "INSERT INTO our_users(first_name, last_name, age, sex, reg_mail)
+      values('Иван', 'Иванов', 23, 'мужской', 'my_mail_21@tut.by')"; 
+sql_msg::result($sql_3);
+ 
+$sql_4 = "INSERT INTO our_users(first_name, last_name, age, sex, reg_mail)
+      values('Елена', 'Сидорова', 23, 'женский', 'my_mail_10@tut.by')"; 
+sql_msg::result($sql_4);
+ 
+$sql_5 = "INSERT INTO our_users(first_name, last_name, age, sex, reg_mail)
+      values('Наталья', 'Осипович', 44, 'женский', 'my_mail_15@tut.by')"; 
+sql_msg::result($sql_5);
  
 //Т.к. больше соединение нам пока не нужно, закрываем его
 $conn->close();   
 ?>
+
