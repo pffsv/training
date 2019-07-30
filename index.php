f5c1cf8acce5d4f9bbc36f0e3d85853262f98ded
pffsv <pffsv@mail.ru>
2019-07-31 05:18:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html>
 <head>
   <meta charset="utf-8">  
-  <title>Отправка простых текстовых данных</title>
+  <title>Отправка данных в массивах</title>
 </head>
 <body>
-  <form action="example_10_2.php" method="POST" name="form_10_1"> 
-    Введите имя  &nbsp; <input type="text" name="firstName"><br><br>
-    Введите фамилию  &nbsp; <input type="text" name="lastName"><br><br>
-    Предпочитаете &nbsp; 
-     <input type="radio" name="user_choice" value="овощи" checked>овощи &nbsp; 
-     <input type="radio" name="user_choice" value="фрукты">фрукты<br><br>
+  <form action="example_10_6.php" method="POST" name="form_10_5"> 
+    фрукты:   <select multiple name="fruits[]" size="3">    
+            <option value="яблоки">Яблоки</option>
+            <option value="апельсины">Апельсины</option>
+            <option value="лимоны">Лимоны</option>
+          </select> <br><br>
     <button type="submit" name="submit" value="send">Отправить</button>
   </form>
 </body>
