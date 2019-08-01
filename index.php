af0d3db8f16bd0febe46c041f616ab8a63838df2
pffsv <pffsv@mail.ru>
2019-08-02 05:28:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
   <title>Загрузка нескольких файлов</title>
 </head>
 <body>
-  <form action="example_9_10.php" method="POST" enctype="multipart/form-data"> 
-    <input type="hidden" name="MAX_FILE_SIZE" value="3000000">
-    Загрузите аватарки (не более 3Мб каждая):<br><br>
-    <input type="file" name="userFile[]" multiple><br><br>
+  <form action="example_10_12.php" method="POST" enctype="multipart/form-data"> 
+    Загрузите фото (не более 1Мб):
+    <input type="hidden" name="MAX_FILE_SIZE" value="1000000">
+    <input type="file"  name="file_1"><br><br>
+    Загрузите аватарку (не более 50Кб):
+    <input type="hidden" name="MAX_FILE_SIZE" value="50000">
+    <input type="file"  name="file_2"><br><br>
     <button type="submit" name="submit" value="send">Отправить</button>
   </form>
 </body>
