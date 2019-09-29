347853fa5692cc6f7e6e7c397fe06b250212f68a
pffsv <pffsv@mail.ru>
2019-09-30 05:02:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,26 @@
 <?php
  
-//Объявляем первый интерфейс
-interface my_intf_1{                  
-  //Метод должен принимать 1 аргумент
-  public function my_func_1($a);     
+//Объявляем первый трейт
+trait my_trait_1{                  
+  //Метод принимает 1 аргумент
+  public function my_func_1($a){  
+    return $a;
+  } 
 }
-  
-//Объявляем второй интерфейс
-interface my_intf_2{                  
-  //Метод должен принимать 1 аргумент
-  public function my_func_2($b);    
-}
-  
-//Реализуем интерфейсы в классе
-class my_class implements my_intf_1, my_intf_2{ 
-                      
-  //Реализуем первый метод
-  public function my_func_1($a){               
-    return $a;    
-  }
  
-  //Реализуем второй метод
-  public function my_func_2($b){               
+//Объявляем второй трейт
+trait my_trait_2{                  
+  //Метод принимает 1 аргумент
+  public function my_func_2($b){  
     return $b;
-  }
+  } 
 }
-  
+ 
+class my_class{
+  //Реализуем трейты в классе
+  use  my_trait_1,  my_trait_2;   
+}
+ 
 $obj= new my_class();          
 //Выведет '1'
 echo $obj->my_func_1(11)%$obj->my_func_2(10);   
