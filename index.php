c0837c8c11c675b81f70cfea4d1e0c660f22307e
pffsv <pffsv@mail.ru>
2019-10-01 05:07:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,35 @@
 <?php
  
 //Объявляем первый трейт
-trait my_trait_1{                  
-  //Метод принимает 1 аргумент
-  public function my_func_1($a){  
+trait my_trait_1{                     
+  //Защищенный метод принимает 1 аргумент
+  protected function my_func_1($a){  
     return $a;
   } 
 }
- 
+  
 //Объявляем второй трейт
-trait my_trait_2{                  
-  //Метод принимает 1 аргумент
-  public function my_func_2($b){  
+trait my_trait_2{                     
+  //Защищенный метод принимает 1 аргумент
+  protected function my_func_1($b){  
     return $b;
   } 
 }
- 
+   
 class my_class{
   //Реализуем трейты в классе
-  use  my_trait_1,  my_trait_2;   
+  use  my_trait_1, my_trait_2{      
+    //Используем метод 1-го трейта
+    my_trait_1::my_func_1 insteadof my_trait_2; 
+    //Расширяем область видимости метода
+    my_trait_1::my_func_1 as public;     
+    //Используем под другим именем
+    my_trait_2::my_func_1 as public my_func_2;  
+  }
 }
- 
+  
 $obj= new my_class();          
 //Выведет '1'
-echo $obj->my_func_1(11)%$obj->my_func_2(10);   
+echo $obj->my_func_1(11)%$obj->my_func_2(10);    
  
-?> 
+?>  
