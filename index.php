93859c03d30b733ca07af05369a06d7e13c82dc5
pffsv <pffsv@mail.ru>
2019-10-02 04:54:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,34 @@
 <?php
  
 //Объявляем первый трейт
-trait my_trait_1{                     
+trait my_trait{                     
   //Защищенный метод принимает 1 аргумент
   protected function my_func_1($a){  
     return $a;
   } 
 }
   
-//Объявляем второй трейт
-trait my_trait_2{                     
-  //Защищенный метод принимает 1 аргумент
-  protected function my_func_1($b){  
-    return $b;
-  } 
+//Объявляем второй интерфейс
+interface my_interface{               
+  //Метод должен принимать 1 аргумент
+  public function my_func_2($b);      
 }
    
-class my_class{
-  //Реализуем трейты в классе
-  use  my_trait_1, my_trait_2{      
-    //Используем метод 1-го трейта
-    my_trait_1::my_func_1 insteadof my_trait_2; 
+$obj= new class implements my_interface{
+  //Реализуем трейт
+  use  my_trait{             
     //Расширяем область видимости метода
-    my_trait_1::my_func_1 as public;     
-    //Используем под другим именем
-    my_trait_2::my_func_1 as public my_func_2;  
+    my_trait::my_func_1 as public;
   }
-}
-  
-$obj= new my_class();          
+ 
+  //Реализуем метод интерфейса
+  public function my_func_2($b){     
+    return $b;
+  }
+   
+}; //Не забываем про точку с запятой 
+           
 //Выведет '1'
-echo $obj->my_func_1(11)%$obj->my_func_2(10);    
+echo $obj->my_func_1(11)%$obj->my_func_2(10); 
  
 ?>  
