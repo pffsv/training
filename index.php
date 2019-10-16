02e4ecaec7476cdef6d2602a76f4a8e610d69528
pffsv <pffsv@mail.ru>
2019-10-16 14:13:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,45 @@
 <?php
  
-//Объявляем первый трейт
-trait my_trait{                     
-  //Защищенный метод принимает 1 аргумент
-  protected function my_func_1($a){  
-    return $a;
-  } 
-}
-  
-//Объявляем второй интерфейс
-interface my_interface{               
-  //Метод должен принимать 1 аргумент
-  public function my_func_2($b);      
-}
-   
-$obj= new class implements my_interface{
-  //Реализуем трейт
-  use  my_trait{             
-    //Расширяем область видимости метода
-    my_trait::my_func_1 as public;
-  }
- 
-  //Реализуем метод интерфейса
-  public function my_func_2($b){     
-    return $b;
+//Объявили пространство имен
+namespace my_name_space{ 
+  //Объявляем константу в текущем пространстве имен 
+  const my_const = 'Константа';    
+     
+  //Объявляем функцию в текущем пространстве имен
+  function my_func($arg){
+    echo $arg.'<br>';
   }
    
-}; //Не забываем про точку с запятой 
-           
-//Выведет '1'
-echo $obj->my_func_1(11)%$obj->my_func_2(10); 
+  //Объявляем класс в текущем пространстве имен
+  class my_class{
+    //Объявили статическое свойство
+    public static $b=5; 
+     
+    //Объявляем общедоступный метод класса
+    public function my_method($arg_1, $arg_2){
+      return $arg_1+$arg_2;
+    }
+  }
+   
+  //Создали объект класса
+  $a=new my_class; 
+   
+  //В текущем пространстве имен можно использовать неполные имена
+  echo my_const.'<br>';
+  my_func(10).'<br>';
+  echo my_class::$b.'<br>';
+  //Переменные не зависят от пространства имен
+  echo $a->my_method(3,5).'<br>';
+}
  
-?>  
+namespace{
+  //Везде добавляем префикс 'my_name_space\' нашего пространства имен,
+  //дополняя до полного имени
+  echo my_name_space\my_const.'<br>';
+  my_name_space\my_func(10).'<br>';
+  echo my_name_space\my_class::$b.'<br>';
+  //Переменные не зависят от пространства имен
+  echo $a->my_method(3,5).'<br>';
+}
+ 
+?> 
