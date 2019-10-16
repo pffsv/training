9c3afe8d35dba64ac927a130062d0f8b0d355a69
pffsv <pffsv@mail.ru>
2019-10-17 05:35:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,42 +4,33 @@
 namespace my_name_space{ 
   //Объявляем константу в текущем пространстве имен 
   const my_const = 'Константа';    
-     
+  //Присвоили переменной число  
+  $b=7; 
+   
   //Объявляем функцию в текущем пространстве имен
   function my_func($arg){
-    echo $arg.'<br>';
+    return $arg;
   }
    
   //Объявляем класс в текущем пространстве имен
   class my_class{
-    //Объявили статическое свойство
-    public static $b=5; 
-     
-    //Объявляем общедоступный метод класса
-    public function my_method($arg_1, $arg_2){
-      return $arg_1+$arg_2;
+    //Объявляем статический метод класса
+    public static function my_method($arg_1, $arg_2){
+      echo $arg_1*$arg_1+$arg_2*$arg_2;
     }
   }
-   
-  //Создали объект класса
-  $a=new my_class; 
-   
-  //В текущем пространстве имен можно использовать неполные имена
-  echo my_const.'<br>';
-  my_func(10).'<br>';
-  echo my_class::$b.'<br>';
-  //Переменные не зависят от пространства имен
-  echo $a->my_method(3,5).'<br>';
 }
  
-namespace{
-  //Везде добавляем префикс 'my_name_space\' нашего пространства имен,
-  //дополняя до полного имени
-  echo my_name_space\my_const.'<br>';
-  my_name_space\my_func(10).'<br>';
-  echo my_name_space\my_class::$b.'<br>';
-  //Переменные не зависят от пространства имен
-  echo $a->my_method(3,5).'<br>';
+namespace my_name_space\inside{
+  //Импортируем и создаем псевдонимы
+  use const my_name_space\my_const as C;
+  use function my_name_space\my_func as F;
+  use my_name_space\my_class as CL;
+   
+  //Теперь просто используем псевдонимы
+  echo C.'<br>';
+  echo F(10).'<br>';
+  CL::my_method(3, 5);
 }
  
 ?> 
