342e3a5caa6b81db532a9156ff1ccb34486c7830
pffsv <pffsv@mail.ru>
2019-10-27 04:13:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,55 +4,31 @@
 //Мой сервер находится в Минске
 date_default_timezone_set('Europe/Minsk');
  
-//Получаем текущую дату (массив)
+//Получаем текущую дату (время сервера)
 $s_d=getdate();
  
-//Переводим дни недели на русский
-switch(true){
-   
-  case $s_d['weekday']=='Monday':
-    $s_day_ru='Понедельник';
-  break;
- 
-  case $s_d['weekday']=='Tuesday':
-    $s_day_ru='Вторник';
-  break;
-   
-  case $s_d['weekday']=='Wednesday':
-    $s_day_ru='Среда';
-  break;
-   
-  case $s_d['weekday']=='Thursday':
-    $s_day_ru='Четверг';
-  break;
-   
-  case $s_d['weekday']=='Friday':
-    $s_day_ru='Пятница';
-  break;
-   
-  case $s_d['weekday']=='Saturday':
-    $s_day_ru='Суббота';
-  break;
-   
-  case $s_d['weekday']=='Sunday':
-    $s_day_ru='Воскресенье';
-  break;
-   
-  default: $s_day_ru='';
-}
- 
-//Заносим в переменную порядковый номер месяца
-$m=$s_d['mon'];
- 
-//Добавляем нули к месяцам
-if($m<10){
-  $m='0'.$m;
-}
- 
-echo <<<HD
-Минское время<br>
-Дата: {$s_d['mday']}.{$m}.{$s_d['year']} ({$s_day_ru})<br>
-Время: {$s_d['hours']}ч. {$s_d['minutes']}мин. {$s_d['seconds']}сек.
+//Составляем строку с датой
+$visit_time = <<<HD
+Прошлый раз вы посещали страницу 
+{$s_d['mday']}.{$s_d['mon']}.{$s_d['year']}
+в {$s_d['hours']}ч. {$s_d['minutes']}мин. {$s_d['seconds']}сек. 
+по Минскому времени.
 HD;
+ 
+//Устанавливаем куки на 10 секунд, но оно будет доступно
+//в массиве только при следующем посещении, пока же 
+//там содержится старое значение
+setcookie('last_vst',$visit_time,time()+10); 
+ 
+//Проверяем, было ли уже установлено cookie, если да, то
+if(isset($_COOKIE['last_vst'])){  
+  //Выводим сообщение
+  echo $_COOKIE['last_vst'];
+//Если же посещение первое или куки истекли
+}else{
+  //Выводим сообщение
+  echo "Вы либо давно к нам не заходили, 
+     либо посещаете нас впервые!";
+}
  
 ?>
