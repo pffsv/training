f4b83869673d46c2db70330a6537afa5d014778d
pffsv <pffsv@mail.ru>
2019-11-06 04:59:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
+<!DOCTYPE html>
 <html>
 <head>
    <meta charset="utf-8">  
-   <title>Отправка простых текстовых данных</title>
+   <title>Загрузка файлов на сервер</title>
 </head>
 <body>
-   <form action="php/example_89.php" method="POST" name="form_89"> 
+   <form action="php/example_90.php" method="POST" enctype="multipart/form-data"> 
     Введите имя  &nbsp; <input type="text" name="first_name"><br><br>
     Введите фамилию  &nbsp; <input type="text" name="last_name"><br><br>
  
-    Укажите ваш возраст: &nbsp; 
-    <!-- Создаем раскрывающийся список -->
-    <select name="age">
-      <option value="до 18 лет">до 18 лет</option>
-      <option value="19-30 лет">19-30 лет</option>
-      <option value="30-50 лет">30-50 лет</option>
-      <option value="50 лет и старше">50 лет и старше</option>
-    </select> <br><br> 
- 
-    Предпочитаете &nbsp; 
-    <input type="radio" name="user_choice" value="овощи" checked>овощи &nbsp; 
-    <input type="radio" name="user_choice" value="фрукты">фрукты<br><br>
+    Загрузите аватарку:<br><br>
+    <input type="file" name="avatar"><br><br>
+     
     <button type="submit" name="submit" value="send">Отправить</button>
+    <button type="reset" name="reset" value="send">Сброс</button>
    </form>
 </body>
 </html>
